dfa1effb0ce6db317cd11a856c413d34ad870fd0
Matt Bruzek <matthew.bruzek@canonical.com>
2016-12-09 11:11:21 -0600

    Adding a cross builder, and improving the all builder.

--- jenkins-cross-builder.sh
+++ jenkins-cross-builder.sh
@@ -70,28 +70,35 @@ TARGETS="cmd/kube-dns
   test/e2e/e2e.test
   test/e2e_node/e2e_node.test"
 # Only build the targets we are interested in.
-${BUILD_DIR}/run.sh make all WHAT="${TARGETS}"
+${BUILD_DIR}/run.sh make cross WHAT="${TARGETS}"
 
 echo "Build finished `date`"
 
 OS=$(get_os)
-ARCH=$(get_arch)
-OUTPUT_DIRECTORY=${KUBE_ROOT}/_output/dockerized/bin/${OS}/${ARCH}
+ARCHITECTURES="amd64 arm64 ppc64le s390x"
+# Iterate over the supported architectures.
+for ARCH in ${ARCHITECTURES}; do
+  OUTPUT_DIRECTORY=${KUBE_ROOT}/_output/dockerized/bin/${OS}/${ARCH}
+  # Check if the architecture specific directory exists.
+  if [ -d ${OUTPUT_DIRECTORY} ]; then
+    E2E_ARCHIVE=${SCRIPT_DIR}/e2e-${VERSION}-${ARCH}.tar.gz
+    echo "Creating the ${E2E_ARCHIVE} file."
+    E2E_FILES="kubectl ginkgo e2e.test e2e_node.test"
+    create_archive ${OUTPUT_DIRECTORY} ${E2E_ARCHIVE} "${E2E_FILES}"
 
-E2E_ARCHIVE=${SCRIPT_DIR}/e2e-${VERSION}-${ARCH}.tar.gz
-echo "Creating the ${E2E_ARCHIVE} file."
-E2E_FILES="kubectl ginkgo e2e.test e2e_node.test"
-create_archive ${OUTPUT_DIRECTORY} ${E2E_ARCHIVE} "${E2E_FILES}"
+    MASTER_ARCHIVE=${SCRIPT_DIR}/kubernetes-master-${VERSION}-${ARCH}.tar.gz
+    echo "Creating the ${MASTER_ARCHIVE} file."
+    MASTER_FILES="kube-apiserver kube-controller-manager kubectl kube-dns kube-scheduler"
+    create_archive ${OUTPUT_DIRECTORY} ${MASTER_ARCHIVE} "${MASTER_FILES}"
 
-MASTER_ARCHIVE=${SCRIPT_DIR}/kubernetes-master-${VERSION}-${ARCH}.tar.gz
-echo "Creating the ${MASTER_ARCHIVE} file."
-MASTER_FILES="kube-apiserver kube-controller-manager kubectl kube-dns kube-scheduler"
-create_archive ${OUTPUT_DIRECTORY} ${MASTER_ARCHIVE} "${MASTER_FILES}"
-
-WORKER_ARCHIVE=${SCRIPT_DIR}/kubernetes-worker-${VERSION}-${ARCH}.tar.gz
-echo "Creating the ${WORKER_ARCHIVE} file."
-WORKER_FILES="kubectl kubelet kube-proxy"
-create_archive ${OUTPUT_DIRECTORY} ${WORKER_ARCHIVE} "${WORKER_FILES}"
+    WORKER_ARCHIVE=${SCRIPT_DIR}/kubernetes-worker-${VERSION}-${ARCH}.tar.gz
+    echo "Creating the ${WORKER_ARCHIVE} file."
+    WORKER_FILES="kubectl kubelet kube-proxy"
+    create_archive ${OUTPUT_DIRECTORY} ${WORKER_ARCHIVE} "${WORKER_FILES}"
+  else
+    echo "Missing architecture: ${ARCH}"
+  fi
+done
 
 # Change back to the original directory.
 cd ${SCRIPT_DIR}
