41036505e1702c1482cd0e48e3fb94a290649a21
Matt Bruzek <matthew.bruzek@canonical.com>
2016-12-13 21:55:54 -0600

    Reverting a breaking change.

--- define-juju.sh
+++ define-juju.sh
@@ -2,6 +2,7 @@
 
 # A function to run a command in the charmbox container.
 function in-charmbox() {
+  local command=$@
   docker run \
     --rm \
     -v ${JUJU_DATA}:/home/ubuntu/.local/share/juju \
@@ -9,27 +10,30 @@ function in-charmbox() {
     -v ${WORKSPACE}:/home/ubuntu/workspace \
     --entrypoint /bin/bash \
     jujusolutions/charmbox:latest \
-    -c "$@"
+    -c "${command}"
 }
 
 # A function to make charm commands run inside a container.
 function charm() {
-  in-charmbox charm "$@"
+  local args=$@
+  in-charmbox charm ${args}
 }
 
 # A function to run a command in the jujubox container.
 function in-jujubox() {
+  local command=$@
   docker run \
     --rm \
     -v ${JUJU_DATA}:/home/ubuntu/.local/share/juju \
     -v ${WORKSPACE}:/home/ubuntu/workspace \
     --entrypoint /bin/bash \
     jujusolutions/jujubox:latest \
-    -c "$@"
+    -c "${command}"
 }
 
 # A function to make juju commands run inside a container.
 function juju() {
+  local args=$@
   # Call the function that runs the commands in a jujubox container.
-  in-jujubox juju "$@"
+  in-jujubox juju ${args}
 }
