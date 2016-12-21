4a95c9eeddaa7deb85d90c02493fc61470fccd03
Matt Bruzek <matthew.bruzek@canonical.com>
2016-12-20 18:08:22 -0600

    Making the code look like the other scripts.

--- jenkins-build-resources.sh
+++ jenkins-build-resources.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 # Builds the kubernetes project to create the output needed for the Juju Charms.
 
+# The first argument ($1) is the EasyRSA version.
+# The second argument ($2) is the flannel version.
+# The third argument ($3) is the CNI version.
+# The fourth argument ($4) is the etcd version.
+# The fifth argument ($5) is the Kubernetes version.
+
 set -o errexit  # Exit when an individual command fails.
 set -o pipefail  # The exit status of the last command is returned.
 #set -o xtrace  # Print the commands that are executed.
 
-# The first argument is the EasyRSA version
 EASYRSA_VERSION=${1:-"3.0.1"}
-# The second argument is the Fflannel version
 FLANNEL_VERSION=${2:-"v0.6.2"}
-# The third argument is the CNI version.
 CNI_VERSION=${3:-"v0.3.0"}
-# The fourth argument is the Etcd version.
 ETCD_VERSION=${4:-"v2.2.5"}
-# The fifth argument is the Kubernetes version.
 KUBE_VERSION=${5:-"v1.5.1"}
 
 SCRIPT_DIR=${PWD}
