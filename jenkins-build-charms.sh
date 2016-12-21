2bc4c2b6e6e1adfdd67ef1eeae8922d99099fcdb
Matt Bruzek <matthew.bruzek@canonical.com>
2016-12-21 13:58:50 -0600

    Renaming the charm build script to match others.

---
+++ jenkins-build-charms.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Runs the charm build for the Canonical Kubernetes charms.
+
+set -o errexit  # Exit when an individual command fails.
+set -o nounset  # Exit when undeclaried variables are used.
+set -o pipefail  # The exit status of the last command is returned.
+set -o xtrace  # Print the commands that are executed.
+
+# The path to the archive of the JUJU_DATA directory for the specific cloud.
+JUJU_DATA_TAR="/var/lib/jenkins/juju/juju_${CLOUD}.tar.gz"
+# Uncompress the file that contains the Juju data to the workspace directory.
+tar -xvzf ${JUJU_DATA_TAR} -C ${WORKSPACE}
+
+# Set the JUJU_DATA directory for this jenkins workspace.
+export JUJU_DATA=${WORKSPACE}/juju
+export JUJU_REPOSITORY=${WORKSPACE}/charms
+
+./git-clone-charm-build.sh
+
+echo "Deploy the locally built charms."
+
+echo "Relate the charms."
+
+echo "Attach the resources."
