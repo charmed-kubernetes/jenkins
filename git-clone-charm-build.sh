a331f4344da41e195ed8878ada827067c1d47a25
Matt Bruzek <mbruzek@gmail.com>
2017-02-27 11:12:07 -0600

    Adding in the KUBE_VERSION to building master.

--- git-clone-charm-build.sh
+++ git-clone-charm-build.sh
@@ -13,7 +13,7 @@ SCRIPT_DIR=${PWD}
 source ./define-juju.sh
 
 # Clone the charm repositories to the current directory.
-git clone https://github.com/juju-solutions/layer-easyrsa.git 
+git clone https://github.com/juju-solutions/layer-easyrsa.git
 git clone https://github.com/juju-solutions/layer-etcd.git
 git clone https://github.com/juju-solutions/charm-flannel.git
 
@@ -22,7 +22,7 @@ KUBERNETES_REPOSITORY=${KUBERNETES_REPOSITORY:-"https://github.com/kubernetes/ku
 
 if [ ! -d kubernetes ]; then
   git clone ${KUBERNETES_REPOSITORY} kubernetes
-fi 
+fi
 # The kubernetes repository contains several charm layers.
 cd kubernetes
 # If the kubernetes branch environment variable is defined use it, otherwise build from master.
@@ -35,13 +35,18 @@ cd ${SCRIPT_DIR}
 # Change the ownership of the charms directory to ubuntu user.
 in-charmbox "sudo chown -R ubuntu:ubuntu /home/ubuntu/charms"
 # Build the charms with no local layers
-CHARM_BUILD_CMD="charm build -r --no-local-layers --force" 
+CHARM_BUILD_CMD="charm build -r --no-local-layers --force"
 in-charmbox "cd workspace/layer-easyrsa && ${CHARM_BUILD_CMD}"
 in-charmbox "cd workspace/layer-etcd && ${CHARM_BUILD_CMD}"
 in-charmbox "cd workspace/charm-flannel && ${CHARM_BUILD_CMD}"
 in-charmbox "cd workspace/kubernetes/cluster/juju/layers/kubeapi-load-balancer && ${CHARM_BUILD_CMD}"
 in-charmbox "cd workspace/kubernetes/cluster/juju/layers/kubernetes-e2e && ${CHARM_BUILD_CMD}"
-in-charmbox "cd workspace/kubernetes/cluster/juju/layers/kubernetes-master && ${CHARM_BUILD_CMD}"
+if [ -n "${KUBE_VERSION}" ]; then
+  MASTER="cd workspace/kubernetes/cluster/juju/layers/kubernetes-master && export KUBE_VERSION=${KUBE_VERSION} && ${CHARM_BUILD_CMD}"
+else
+  MASTER="cd workspace/kubernetes/cluster/juju/layers/kubernetes-master && ${CHARM_BUILD_CMD}"
+fi
+in-charmbox "${MASTER}"
 in-charmbox "cd workspace/kubernetes/cluster/juju/layers/kubernetes-worker && ${CHARM_BUILD_CMD}"
 # Change the ownership of the charms directory to ubuntu user.
 in-charmbox "sudo chown -R ubuntu:ubuntu /home/ubuntu/charms"
