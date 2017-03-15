99612d66ff2450257cde2639b7ce3575465ab8bb
Matt Bruzek <matthew.bruzek@canonical.com>
2017-03-15 14:18:35 -0500

    Making the cleanup work more better.

--- jenkins-e2e.sh
+++ jenkins-e2e.sh
@@ -6,14 +6,21 @@ set -o nounset  # Exit when undeclaried variables are used.
 set -o pipefail  # The exit status of the last command is returned.
 set -o xtrace  # Print the commands that are executed.
 
-# The path to the archive of the JUJU_DATA directory for the specific cloud.
-JUJU_DATA_TAR="/var/lib/jenkins/juju/juju_${CLOUD}.tar.gz"
-# Uncompress the file that contains the Juju data to the workspace directory.
-tar -xvzf ${JUJU_DATA_TAR} -C ${WORKSPACE}
-
+if [[ -z "${WORKSPACE}" ]]; then
+  export WORKSPACE=${PWD}
+fi
 # Set the Juju envrionment variables for this jenkins job.
 export JUJU_DATA=${WORKSPACE}/juju
 export JUJU_REPOSITORY=${WORKSPACE}/charms
+
+mkdir ${JUJU_DATA}
+mkdir ${JUJU_REPOSITORY}
+
+# The path to the archive of the JUJU_DATA directory for the specific cloud.
+JUJU_DATA_TAR="/var/lib/jenkins/juju/juju_${CLOUD}.tar.gz"
+
+# Uncompress the file that contains the Juju data to the workspace directory.
+tar -xvzf ${JUJU_DATA_TAR} -C ${WORKSPACE}
 
 # Define a unique model name for this run.
 MODEL=${BUILD_TAG}
@@ -29,7 +36,7 @@ USER_ID=$(id -u)
 # Change the permissions back to the current user so jenkins can clean up.
 CHOWN_CMD="sudo chown -R ${USER_ID}:${GROUP_ID} /home/ubuntu/.local/share/juju"
 # Catch all EXITs from this script and make sure to destroy the model.
-trap "juju destroy-model -y ${MODEL} || in-jujubox ${CHOWN_CMD} || true" EXIT
+trap "juju destroy-model -y ${MODEL} && in-jujubox ${CHOWN_CMD}" EXIT
 
 # Deploy the bundle and add the kubernetes-e2e charm.
 ./juju-deploy-test-bundle.sh ${MODEL} ${BUNDLE}
