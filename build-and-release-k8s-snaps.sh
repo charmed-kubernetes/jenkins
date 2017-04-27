e71525b4df162135a3ba0feb88654eb17951be6a
Rye Terrell <ryeterrell@ryeterrell.net>
2017-04-27 15:03:56 -0500

    push cdk-addons to the snap store (#13)

--- build-and-release-k8s-snaps.sh
+++ build-and-release-k8s-snaps.sh
@@ -8,3 +8,10 @@ cd release/snap && make KUBE_VERSION=$KUBE_VERSION
 for app in kubeadm kube-apiserver kube-controller-manager kubectl kubefed kubelet kube-proxy kube-scheduler kubernetes-test; do
   snapcraft push build/${app}_${KUBE_VERSION:1}_amd64.snap --release ${KUBE_VERSION:1:3}/edge
 done
+
+cd ../..
+
+git clone https://github.com/juju-solutions/cdk-addons.git --depth 1
+cd cdk-addons && make KUBE_VERSION=$KUBE_VERSION
+
+snapcraft push build/cdk-addons_${KUBE_VERSION:1}_amd64.snap --release ${KUBE_VERSION:1:3}/edge
