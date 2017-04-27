8c67f4044dd3fe382f1e3bc5c11fb7ba44288162
Rye Terrell <ryeterrell@ryeterrell.net>
2017-04-27 13:09:23 -0500

    push snaps to edge (#12)
    * push snaps to edge
    
    * push kubernetes-test

---
+++ build-and-release-k8s-snaps.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eux
+
+git clone https://github.com/juju-solutions/release.git --branch rye/snaps --depth 1
+cd release/snap && make KUBE_VERSION=$KUBE_VERSION
+
+for app in kubeadm kube-apiserver kube-controller-manager kubectl kubefed kubelet kube-proxy kube-scheduler kubernetes-test; do
+  snapcraft push build/${app}_${KUBE_VERSION:1}_amd64.snap --release ${KUBE_VERSION:1:3}/edge
+done
--- jenkins-build-k8s-snaps.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-git clone https://github.com/juju-solutions/release.git --branch rye/snaps --depth 1
-cd release/snap && ./docker-build.sh KUBE_VERSION=$KUBE_VERSION
