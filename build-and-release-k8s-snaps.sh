40162dc51baea0490ce428765db21cdb3f577ffb
Rye Terrell <ryeterrell@ryeterrell.net>
2017-04-28 13:59:41 -0500

    multi-arch cdk-addons (#15)

--- build-and-release-k8s-snaps.sh
+++ build-and-release-k8s-snaps.sh
@@ -13,7 +13,9 @@ git clone https://github.com/juju-solutions/release.git --branch rye/snaps --dep
 )
 
 git clone https://github.com/juju-solutions/cdk-addons.git --depth 1
-(cd cdk-addons && make KUBE_VERSION=$KUBE_VERSION)
+for arch in $KUBE_ARCH; do
+  (cd cdk-addons && make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH=${arch})
+done
 
 for app in kubeadm kube-apiserver kubectl kubefed kubelet kube-proxy kube-scheduler; do
   for arch in $KUBE_ARCH; do
@@ -25,4 +27,6 @@ for app in kube-controller-manager kubernetes-test; do
   snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_amd64.snap --release ${KUBE_VERSION:1:3}/edge
 done
 
-snapcraft push cdk-addons/build/cdk-addons_${KUBE_VERSION:1}_amd64.snap --release ${KUBE_VERSION:1:3}/edge
+for arch in $KUBE_ARCH; do
+  snapcraft push cdk-addons/cdk-addons_${KUBE_VERSION:1}_${arch}.snap --release ${KUBE_VERSION:1:3}/edge
+done
