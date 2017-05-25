8f2119e42a32329b6a1abfa7eff33d6c974e77d3
George Kraft <cynerva@gmail.com>
2017-05-25 14:53:22 -0500

    Set TEST_RESULT_DIR for use in bundletester

--- tests/run-bundle-tests.sh
+++ tests/run-bundle-tests.sh
@@ -17,9 +17,15 @@ TEST_CONTROLLERS="${TEST_CONTROLLERS:-$(juju switch | cut -d ':' -f 1)}"
 TEST_BUNDLE_CHANNEL="${TEST_BUNDLE_CHANNEL:-edge}"
 TEST_SNAP_CHANNEL="${TEST_SNAP_CHANNEL:-1.6/edge}"
 
+rm -rf results
+
 for controller in $TEST_CONTROLLERS; do
   juju switch "$controller"
   for bundle in $TEST_BUNDLES; do
+    result_dir="results/$(echo $bundle | sed 's|/|_|g')/$controller"
+    mkdir -p "$result_dir"
+    export TEST_RESULT_DIR="$(readlink -f "$result_dir")"
+
     model="cdk-build-$RANDOM"
     juju add-model "$model" --config test-mode=true
     (trap "juju destroy-model -y $model" EXIT
@@ -27,7 +33,7 @@ for controller in $TEST_CONTROLLERS; do
       charm pull canonical-kubernetes --channel "$TEST_BUNDLE_CHANNEL" bundle-under-test
       ./tests/set-snap-channel.py bundle-under-test "$TEST_SNAP_CHANNEL"
 
-      bundletester --no-matrix -vF -l DEBUG -t bundle-under-test -r xml -o bundle-test-results.xml
+      bundletester --no-matrix -vF -l DEBUG -t bundle-under-test -r xml -o "$result_dir/bundletester.xml"
     )
   done
 done
