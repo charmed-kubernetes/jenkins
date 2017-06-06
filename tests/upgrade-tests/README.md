4b71cf36d33da9e6ad2c722e0d9fb5b3e67b7953
George Kraft <cynerva@gmail.com>
2017-06-06 15:12:33 -0500

    Get some docs in there.

---
+++ tests/upgrade-tests/README.md
@@ -0,0 +1,20 @@
+# CDK upgrade tests
+
+## Install
+
+Dependencies can be installed by running:
+```
+./install-deps.sh
+```
+
+## Running tests
+
+To run all tests:
+```
+pytest
+```
+
+Individual test files can be passed to pytest:
+```
+pytest test_upgrade_charms.py
+```
--- tests/upgrade-tests/utils.py
+++ tests/upgrade-tests/utils.py
@@ -7,9 +7,13 @@ from juju.controller import Controller
 
 @async_contextmanager
 async def temporary_model():
+    ''' Create and destroy a temporary Juju model named cdk-build-upgrade-*.
+
+    This is an async context, to be used within an `async with` statement.
+    '''
     controller = Controller()
     await controller.connect_current()
-    model_name = "cdk-build-upgrade-%d" % random.randint(0, 10000)
+    model_name = 'cdk-build-upgrade-%d' % random.randint(0, 10000)
     model = await controller.add_model(model_name)
     try:
         await yield_(model)
@@ -25,6 +29,7 @@ def assert_no_unit_errors(model):
 
 
 def all_units_ready(model):
+    ''' Returns True if all units are 'active' and 'idle', False otherwise. '''
     for unit in model.units.values():
         if unit.data['workload-status']['current'] != 'active':
             return False
@@ -34,8 +39,13 @@ def all_units_ready(model):
 
 
 async def wait_for_ready(model):
-    """ Wait until all units are 'active' and 'idle'. """
-    # FIXME: It's possible this might not handle subordinates properly
+    ''' Wait until all units are 'active' and 'idle'. '''
+    # FIXME: We might need to wait for more than just unit status.
+    #
+    # Subordinate units, for example, don't come into existence until after the
+    # principal unit has settled.
+    #
+    # If you see problems where this didn't wait long enough, it's probably that.
     while not all_units_ready(model):
         assert_no_unit_errors(model)
         await asyncio.sleep(1)
@@ -43,10 +53,11 @@ async def wait_for_ready(model):
 
 
 def assert_healthy(model):
+    ''' Assert that the current deployment is healthy. '''
     assert_no_unit_errors(model)
     expected_messages = {
-      "kubernetes-master": "Kubernetes master running.",
-      "kubernetes-worker": "Kubernetes worker running."
+      'kubernetes-master': 'Kubernetes master running.',
+      'kubernetes-worker': 'Kubernetes worker running.'
     }
     for app, message in expected_messages.items():
         for unit in model.applications[app].units:
