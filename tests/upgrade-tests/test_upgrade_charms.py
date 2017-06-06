9d6086798305e82914226997f4b5daa75a998bb3
George Kraft <cynerva@gmail.com>
2017-06-06 12:11:07 -0500

    Parameterize test cases

--- tests/upgrade-tests/test_upgrade_charms.py
+++ tests/upgrade-tests/test_upgrade_charms.py
@@ -1,18 +1,22 @@
 import pytest
 from utils import temporary_model, wait_for_ready, assert_healthy
 
-CHARMS_UPGRADE_FROM="stable"
-CHARMS_UPGRADE_TO="beta"
+test_cases = [
+    # bundle                 from_channel  to_channel
+    ('kubernetes-core',      'stable',     'beta'),
+    ('canonical-kubernetes', 'stable',     'beta'),
+]
 
 @pytest.mark.asyncio
-async def test_upgrade_charms():
+@pytest.mark.parametrize('bundle,from_channel,to_channel', test_cases)
+async def test_upgrade_charms(bundle, from_channel, to_channel):
     async with temporary_model() as model:
-        await model.deploy("kubernetes-core", channel=CHARMS_UPGRADE_FROM)
+        await model.deploy(bundle, channel=from_channel)
         await wait_for_ready(model)
-        await model.applications["easyrsa"].upgrade_charm(channel=CHARMS_UPGRADE_TO)
-        await model.applications["etcd"].upgrade_charm(channel=CHARMS_UPGRADE_TO)
-        await model.applications["flannel"].upgrade_charm(channel=CHARMS_UPGRADE_TO)
-        await model.applications["kubernetes-master"].upgrade_charm(channel=CHARMS_UPGRADE_TO)
-        await model.applications["kubernetes-worker"].upgrade_charm(channel=CHARMS_UPGRADE_TO)
+        await model.applications['easyrsa'].upgrade_charm(channel=to_channel)
+        await model.applications['etcd'].upgrade_charm(channel=to_channel)
+        await model.applications['flannel'].upgrade_charm(channel=to_channel)
+        await model.applications['kubernetes-master'].upgrade_charm(channel=to_channel)
+        await model.applications['kubernetes-worker'].upgrade_charm(channel=to_channel)
         await wait_for_ready(model)
         assert_healthy(model)
--- tests/upgrade-tests/test_upgrade_snaps.py
+++ tests/upgrade-tests/test_upgrade_snaps.py
@@ -1,18 +1,24 @@
 import pytest
 from utils import temporary_model, wait_for_ready, assert_healthy
 
-SNAPS_UPGRADE_FROM='1.6/stable'
-SNAPS_UPGRADE_TO='1.6/edge'
+test_cases = [
+    # bundle                 from_channel  to_channel
+    ('kubernetes-core',      '1.6/stable', '1.6/edge'),
+    ('kubernetes-core',      '1.6/stable', '1.7/edge'),
+    ('canonical-kubernetes', '1.6/stable', '1.6/edge'),
+    ('canonical-kubernetes', '1.6/stable', '1.7/edge'),
+]
 
 @pytest.mark.asyncio
-async def test_upgrade_snaps():
+@pytest.mark.parametrize('bundle,from_channel,to_channel', test_cases)
+async def test_upgrade_snaps(bundle, from_channel, to_channel):
     async with temporary_model() as model:
-        await model.deploy('kubernetes-core', channel='stable')
-        await model.applications['kubernetes-master'].set_config({'channel': SNAPS_UPGRADE_FROM})
-        await model.applications['kubernetes-worker'].set_config({'channel': SNAPS_UPGRADE_FROM})
+        await model.deploy(bundle, channel='stable')
+        await model.applications['kubernetes-master'].set_config({'channel': from_channel})
+        await model.applications['kubernetes-worker'].set_config({'channel': from_channel})
         await wait_for_ready(model)
-        await model.applications['kubernetes-master'].set_config({'channel': SNAPS_UPGRADE_TO})
-        await model.applications['kubernetes-worker'].set_config({'channel': SNAPS_UPGRADE_TO})
+        await model.applications['kubernetes-master'].set_config({'channel': to_channel})
+        await model.applications['kubernetes-worker'].set_config({'channel': to_channel})
         for unit in model.applications['kubernetes-worker'].units:
             action = await unit.run_action('upgrade')
             await action.wait()
