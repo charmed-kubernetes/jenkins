87431a8e37c46f5abf48553fef436ef508e1edf6
George Kraft <cynerva@gmail.com>
2017-06-06 15:19:48 -0500

    Simplify upgrade charm test. Just upgrade every app in the model.

--- tests/upgrade-tests/test_upgrade_charms.py
+++ tests/upgrade-tests/test_upgrade_charms.py
@@ -7,18 +7,14 @@ test_cases = [
     ('canonical-kubernetes', 'stable',     'beta'),
 ]
 
+
 @pytest.mark.asyncio
 @pytest.mark.parametrize('bundle,from_channel,to_channel', test_cases)
 async def test_upgrade_charms(bundle, from_channel, to_channel):
     async with temporary_model() as model:
         await model.deploy(bundle, channel=from_channel)
         await wait_for_ready(model)
-        await model.applications['easyrsa'].upgrade_charm(channel=to_channel)
-        await model.applications['etcd'].upgrade_charm(channel=to_channel)
-        await model.applications['flannel'].upgrade_charm(channel=to_channel)
-        await model.applications['kubernetes-master'].upgrade_charm(channel=to_channel)
-        await model.applications['kubernetes-worker'].upgrade_charm(channel=to_channel)
-        if 'kubeapi-load-balancer' in model.applications:
-            await model.applications['kubeapi-load-balancer'].upgrade_charm(channel=to_channel)
+        for app in model.applications.values():
+            app.upgrade_charm(channel=to_channel)
         await wait_for_ready(model)
         assert_healthy(model)
