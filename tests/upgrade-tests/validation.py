74cbfc0d2c1988a8d7e8072b4819af169c336358
George Kraft <cynerva@gmail.com>
2017-06-09 15:26:09 -0500

    Validate snap versions are consistent with snap channel config

--- tests/upgrade-tests/validation.py
+++ tests/upgrade-tests/validation.py
@@ -3,6 +3,7 @@ from utils import assert_no_unit_errors
 
 async def validate_all(model):
     validate_status_messages(model)
+    await validate_snap_versions(model)
     await validate_microbot(model)
     assert_no_unit_errors(model)
 
@@ -16,6 +17,40 @@ def validate_status_messages(model):
     for app, message in expected_messages.items():
         for unit in model.applications[app].units:
             assert unit.data['workload-status']['message'] == message
+
+
+async def validate_snap_versions(model):
+    ''' Validate that the installed snap versions are consistent with channel
+    config on the charms.
+    '''
+    snaps_to_validate = {
+        'kubernetes-master': [
+            'kubectl',
+            'kube-apiserver',
+            'kube-controller-manager',
+            'kube-scheduler',
+            'cdk-addons',
+        ],
+        'kubernetes-worker': [
+            'kubectl',
+            'kubelet',
+            'kube-proxy',
+        ],
+    }
+    for app_name, snaps in snaps_to_validate.items():
+        app = model.applications[app_name]
+        config = await app.get_config()
+        channel = config['channel']['value']
+        track = channel.split('/')[0]
+        for unit in app.units:
+            action = await unit.run('snap list')
+            assert action.status == 'completed'
+            raw_output = action.data['results']['Stdout']
+            lines = raw_output.splitlines()[1:]
+            snap_versions = dict(line.split()[:2] for line in lines)
+            for snap in snaps:
+                snap_version = snap_versions[snap]
+                assert snap_version.startswith(track + '.')
 
 
 async def validate_microbot(model):
