a60e904b8a8d4075fd971b45af929321de7f7803
George Kraft <cynerva@gmail.com>
2017-06-20 14:33:35 -0500

    Add python3-pip and conjure-up to install deps

--- tests/upgrade-tests/install-deps.sh
+++ tests/upgrade-tests/install-deps.sh
@@ -4,4 +4,7 @@ set -eux
 # Installs dependencies needed for upgrade tests
 # Can be run again to upgrade dependencies.
 
+sudo apt update
+sudo apt install -y python3-pip
 sudo pip3 install -U pytest pytest-asyncio asyncio_extras juju
+sudo snap install conjure-up --classic
