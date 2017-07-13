bdaee76fb65d1ac43833699c97cf6e84f403204c
George Kraft <cynerva@gmail.com>
2017-07-13 09:57:47 -0500

    Update dependencies

--- integration-tests/install-deps.sh
+++ integration-tests/install-deps.sh
@@ -5,6 +5,7 @@ set -eux
 # Can be run again to upgrade dependencies.
 
 sudo apt update
-sudo apt install -y python3-pip
-sudo pip3 install -U pytest pytest-asyncio asyncio_extras juju requests
+sudo apt install -y python3-pip python-pip
+sudo pip3 install -U pytest pytest-asyncio asyncio_extras juju requests pyyaml
+sudo pip2 install -U bundletester
 sudo snap install conjure-up --classic
