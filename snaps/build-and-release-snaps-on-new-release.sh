297d65a93cacaa6b3728b99ae97c5b9eb61d7a60
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2017-10-24 19:00:20 +0300

    Check for releases on all branches and release snaps (#102)

--- snaps/build-and-release-snaps-on-new-release.sh
+++ snaps/build-and-release-snaps-on-new-release.sh
@@ -3,12 +3,14 @@
 
 set -eux
 
-KUBE_VERSION="${KUBE_VERSION:-$(curl -L https://dl.k8s.io/release/stable.txt)}"
-# LAST_RELEASE_FILE keeps the last release we did.
-LAST_RELEASE_FILE="/var/tmp/last_k8s_patch_release"
-
+# K8s versions we want to check for new releases
+declare -a VERSIONS=('1.6' '1.7' '1.8' '1.9' '2.0' 'latest');
 
 function check_for_release {
+  # Sets $trigger to 'yes' when a new release is available.
+  # Looks inside $LAST_RELEASE_FILE and compares the contents with
+  # what is in $KUBE_VERSION
+
   trigger='no'
   if [ -f $LAST_RELEASE_FILE ]
   then
@@ -30,9 +32,10 @@ function check_for_release {
 }
 
 
-check_for_release
-if [ "$trigger" == 'yes' ]
-then
+function build_promote {
+  # Builds and promotes to edge and candidate the k8s release found in
+  # $KUBE_VERSION. Calls build-and-release-k8s-snaps.sh and promote-snaps.sh.
+
   scripts_path=$(dirname "$0")
   # Build the snaps and push to edge
   $scripts_path/build-and-release-k8s-snaps.sh
@@ -41,8 +44,79 @@ then
   version=${KUBE_VERSION:1:3}
   export PROMOTE_FROM="$version/edge"
   export PROMOTE_TO="$version/beta $version/candidate"
+  if [ "$BRANCH" == "latest" ]
+  then
+    export PROMOTE_TO="edge beta candidate $PROMOTE_TO"
+  fi
   $scripts_path/promote-snaps.sh
 
   # We are done with promoting the snaps. Lets mark the release.
   echo "$KUBE_VERSION" > $LAST_RELEASE_FILE
-fi
+}
+
+function promote_stable {
+  # Promotes to stable the release found in candidate.
+  # The following conditions should hold:
+  # 1. $LAST_RELEASE_FILE should be the same the past 7 days AND
+  # 2. a) There was not previous release ($STABLE_RELEASED_FILE does not exist) OR
+  #    b) There is a new release ($LAST_RELEASE_FILE and $STABLE_RELEASED_FILE differ)
+
+  seven_days_ago=$(date -d 'now - 7 days' +%s)
+  last_release_time=$(date -r "$LAST_RELEASE_FILE" +%s)
+  stable_promotion='no'
+  if (( last_release_time >= seven_days_ago))
+  then
+    echo "Release $KUBE_VERSION too young to promote to stable"
+  elif [ ! -f $STABLE_RELEASE_FILE ] || ! cmp --silent $LAST_RELEASE_FILE $STABLE_RELEASE_FILE
+  then
+    echo "Promoting mature $KUBE_VERSION release to stable"
+    # Promote snaps from edge to candidate
+    scripts_path=$(dirname "$0")
+    version=${KUBE_VERSION:1:3}
+    export PROMOTE_FROM="$version/candidate"
+    export PROMOTE_TO="$version/stable"
+    if [ "$BRANCH" == "latest" ]
+    then
+      export PROMOTE_TO="stable $PROMOTE_TO"
+    fi
+    $scripts_path/promote-snaps.sh
+
+    stable_promotion='yes'
+  fi
+}
+
+
+# Main loop. Go over all versions
+for BRANCH in ${VERSIONS[@]}
+do
+  if [ "$BRANCH" == "latest" ]
+  then
+    url="https://dl.k8s.io/release/stable.txt"
+  else
+    url="https://dl.k8s.io/release/stable-${BRANCH}.txt"
+  fi
+  export BRANCH
+  export KUBE_VERSION="$(curl -s -L $url)"
+  # LAST_RELEASE_FILE keeps the last release we did to candidate.
+  export LAST_RELEASE_FILE="/var/tmp/last_${BRANCH}_k8s_patch_release"
+  # STABLE_RELEASED_FILE keeps the last release we did to candidate.
+  export STABLE_RELEASE_FILE="/var/tmp/last_${BRANCH}_k8s_patch_release.to_stable"
+
+  # Work only on the available branches (eg 2.0 might not be there yet)
+  if [[ $KUBE_VERSION != *"Error"* ]]
+  then
+    echo "Processing $KUBE_VERSION"
+    check_for_release
+    if [ "$trigger" == 'yes' ]
+    then
+      build_promote
+      echo "$KUBE_VERSION" > $LAST_RELEASE_FILE
+    fi
+
+    promote_stable
+    if [ "$stable_promotion" == 'yes' ]
+    then
+      echo "$KUBE_VERSION" > $STABLE_RELEASE_FILE
+    fi
+  fi
+done
