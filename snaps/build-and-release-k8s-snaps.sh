90b0709766879122ce4952b5f26aa454495f80e8
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2017-10-27 23:24:36 +0300

    Use main major_minor version extraction

--- snaps/build-and-release-k8s-snaps.sh
+++ snaps/build-and-release-k8s-snaps.sh
@@ -4,10 +4,12 @@ set -eux
 
 KUBE_VERSION="${KUBE_VERSION:-$(curl -L https://dl.k8s.io/release/stable.txt)}"
 KUBE_ARCH="amd64"
-ADDONS_BRANCH_VERSION="release-${KUBE_VERSION:1:3}"
+
+source utilities.sh
+MAIN_VERSION=$(get_major_minor $KUBE_VERSION)
+ADDONS_BRANCH_VERSION="release-${MAIN_VERSION}"
 
 source utils/retry.sh
-source utilities.sh
 
 rm -rf ./release
 git clone https://github.com/juju-solutions/release.git --branch rye/snaps --depth 1
@@ -31,17 +33,16 @@ for arch in $KUBE_ARCH; do
   (cd cdk-addons && make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH=${arch})
 done
 
-channel=$(get_major_minor $KUBE_VERSION)
 for app in kubeadm kube-apiserver kubectl kubefed kubelet kube-proxy kube-scheduler; do
   for arch in $KUBE_ARCH; do
-    retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_${arch}.snap --release ${channel}/edge
+    retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_${arch}.snap --release ${MAIN_VERSION}/edge
   done
 done
 
 for app in kube-controller-manager kubernetes-test; do
-  retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_amd64.snap --release ${channel}/edge
+  retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_amd64.snap --release ${MAIN_VERSION}/edge
 done
 
 for arch in $KUBE_ARCH; do
-  retry snapcraft push cdk-addons/cdk-addons_${KUBE_VERSION:1}_${arch}.snap --release ${channel}/edge
+  retry snapcraft push cdk-addons/cdk-addons_${KUBE_VERSION:1}_${arch}.snap --release ${MAIN_VERSION}/edge
 done
