43c72193263f108d3b826a8c681d03cdfc425c05
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2017-10-31 15:06:26 +0200

    Build cdk addons from release branches (#107)
    * Build cdk-addons from branches if they exist.
    
    * Adding extra print out of cdk-addons branch
    
    * Use main major_minor version extraction

--- snaps/build-and-release-k8s-snaps.sh
+++ snaps/build-and-release-k8s-snaps.sh
@@ -5,8 +5,11 @@ set -eux
 KUBE_VERSION="${KUBE_VERSION:-$(curl -L https://dl.k8s.io/release/stable.txt)}"
 KUBE_ARCH="amd64"
 
-source utils/retry.sh
 source utilities.sh
+MAIN_VERSION=$(get_major_minor $KUBE_VERSION)
+ADDONS_BRANCH_VERSION="release-${MAIN_VERSION}"
+
+source utils/retry.sh
 
 rm -rf ./release
 git clone https://github.com/juju-solutions/release.git --branch rye/snaps --depth 1
@@ -18,22 +21,28 @@ git clone https://github.com/juju-solutions/release.git --branch rye/snaps --dep
 )
 
 rm -rf ./cdk-addons
-git clone https://github.com/juju-solutions/cdk-addons.git --depth 1
+if git ls-remote --exit-code --heads https://github.com/juju-solutions/cdk-addons.git ${ADDONS_BRANCH_VERSION}
+then
+  echo "Getting cdk-addons from ${ADDONS_BRANCH_VERSION} branch."
+  git clone https://github.com/juju-solutions/cdk-addons.git --branch ${ADDONS_BRANCH_VERSION} --depth 1
+else
+  echo "Branch for ${ADDONS_BRANCH_VERSION} does not exist. Getting cdk-addons from master head."
+  git clone https://github.com/juju-solutions/cdk-addons.git --depth 1
+fi
 for arch in $KUBE_ARCH; do
   (cd cdk-addons && make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH=${arch})
 done
 
-channel=$(get_major_minor $KUBE_VERSION)
 for app in kubeadm kube-apiserver kubectl kubefed kubelet kube-proxy kube-scheduler; do
   for arch in $KUBE_ARCH; do
-    retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_${arch}.snap --release ${channel}/edge
+    retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_${arch}.snap --release ${MAIN_VERSION}/edge
   done
 done
 
 for app in kube-controller-manager kubernetes-test; do
-  retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_amd64.snap --release ${channel}/edge
+  retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_amd64.snap --release ${MAIN_VERSION}/edge
 done
 
 for arch in $KUBE_ARCH; do
-  retry snapcraft push cdk-addons/cdk-addons_${KUBE_VERSION:1}_${arch}.snap --release ${channel}/edge
+  retry snapcraft push cdk-addons/cdk-addons_${KUBE_VERSION:1}_${arch}.snap --release ${MAIN_VERSION}/edge
 done
