8e994e3316b3a54e37e9971314d13a3bc76158bc
George Kraft <cynerva@gmail.com>
2017-10-31 17:26:14 -0500

    Update test_live_model to wait for ready before validation

--- integration-tests/test_live_model.py
+++ integration-tests/test_live_model.py
@@ -3,8 +3,9 @@
 
 import pytest
 from juju.model import Model
-from utils import captured_fail_logs
+from utils import captured_fail_logs, wait_for_ready
 from validation import validate_all
+
 
 @pytest.mark.asyncio
 async def test_live_model(log_dir):
@@ -12,6 +13,7 @@ async def test_live_model(log_dir):
     try:
         await model.connect_current()
         async with captured_fail_logs(model, log_dir):
+            await wait_for_ready(model)
             await validate_all(model, log_dir)
     finally:
         await model.disconnect()
