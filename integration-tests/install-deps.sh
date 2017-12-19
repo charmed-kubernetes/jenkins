ed733eb1ba5d9993b54d92c8c9f38662628adecd
George Kraft <cynerva@gmail.com>
2017-12-19 09:20:56 -0600

    Install amulet from git/master (#135)

--- integration-tests/install-deps.sh
+++ integration-tests/install-deps.sh
@@ -10,9 +10,10 @@ sudo snap install juju --classic
 sudo snap install conjure-up --classic
 sudo pip2 install 'git+https://github.com/juju/juju-crashdump'
 sudo pip2 install -U pyopenssl bundletester
-sudo pip3 install -U pytest  pytest-asyncio asyncio_extras juju requests pyyaml kubernetes amulet
+sudo pip3 install -U pytest  pytest-asyncio asyncio_extras juju requests pyyaml kubernetes
+sudo pip3 install -U 'git+https://github.com/juju/amulet' # we need https://github.com/juju/amulet/pull/183
+
 # Leaving those here in case we need to build a client from bleeding edge
 # sudo pip2 install 'git+https://github.com/juju-solutions/bundletester' \
 #                 'git+https://github.com/juju/juju-crashdump'
 # sudo pip3 install 'git+https://github.com/juju/python-libjuju'
-#                  'git+https://github.com/juju/amulet'
