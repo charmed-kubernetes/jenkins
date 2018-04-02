61ad8ea767e5a973d63ffcadca57910fb568d69b
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-04-02 16:45:25 +0300

    Revert "install charm-tools with snapd (#152)" (#154)
    This reverts commit c129f237ee5bcf1a5d268f50ed69705beb7fffbf.

--- integration-tests/install-deps.sh
+++ integration-tests/install-deps.sh
@@ -5,9 +5,8 @@ set -eux
 # Can be run again to upgrade dependencies.
 
 sudo apt update -yq
-sudo apt install -y unzip python3-pip python-pip squashfuse snapd
+sudo apt install -y unzip python3-pip python-pip squashfuse snapd charm-tools
 sudo snap install juju --classic
-sudo snap install charm
 sudo snap install conjure-up --classic
 sudo pip2 install 'git+https://github.com/juju/juju-crashdump'
 sudo pip2 install -U pyopenssl bundletester
