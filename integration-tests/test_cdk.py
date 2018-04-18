4f0c85f683ad6ed595d1a2ed1c1ac99f9c070185
Mike Wilson <knobby@burntsheep.com>
2018-04-18 15:33:33 -0400

    Fixing logic for upgrade test
    I had it with a new variable for the upgrade to channel, but the existing variable WAS an upgrade to variable, so this should be an upgrade from variable instead. Swapping that around and renaming some variables to hopefully clarify their meanings.

--- integration-tests/test_cdk.py
+++ integration-tests/test_cdk.py
@@ -5,12 +5,12 @@ from utils import juju_deploy, run_bundletester, default_bundles
 from validation import validate_all
 
 namespace = os.environ.get('TEST_CHARM_NAMESPACE', 'containers')
-charm_channel = os.environ.get('TEST_CHARM_CHANNEL', 'stable')
-snap_channel = os.environ.get('TEST_SNAP_CHANNEL', '1.9/stable')
+test_charm_channel = os.environ.get('TEST_CHARM_CHANNEL', 'stable')
+test_snap_channel = os.environ.get('TEST_SNAP_CHANNEL', '1.9/stable')
 test_cloud = os.environ.get('TEST_CLOUD', '')
 bundles_csv = os.environ.get('TEST_BUNDLES', default_bundles())
-upgrade_snap_channel = os.environ.get('UPGRADE_SNAP_CHANNEL', 'stable')
-upgrade_charm_channel = os.environ.get('UPGRADE_CHARM_CHANNEL', 'edge')
+upgrade_from_snap_channel = os.environ.get('UPGRADE_FROM_SNAP_CHANNEL', 'stable')
+upgrade_from_charm_channel = os.environ.get('UPGRADE_FROM_CHARM_CHANNEL', 'edge')
 bundles = [bundle.strip() for bundle in bundles_csv.split(',')]
 
 
@@ -18,11 +18,11 @@ bundles = [bundle.strip() for bundle in bundles_csv.split(',')]
 @pytest.mark.parametrize('bundle', bundles)
 async def test_deploy(bundle, log_dir):
     async with temporary_model(log_dir, force_cloud=test_cloud) as model:
-        # await conjureup(model, namespace, bundle, charm_channel,
+        # await conjureup(model, namespace, bundle, test_charm_channel,
         #                 snap_channel)
-        await juju_deploy(model, namespace, bundle, charm_channel,
-                          snap_channel)
-        await deploy_e2e(model, charm_channel, snap_channel,
+        await juju_deploy(model, namespace, bundle, test_charm_channel,
+                          test_snap_channel)
+        await deploy_e2e(model, test_charm_channel, test_snap_channel,
                          namespace=namespace)
         await validate_all(model, log_dir)
 
@@ -32,16 +32,17 @@ async def test_deploy(bundle, log_dir):
 async def test_upgrade(bundle, log_dir):
     async with temporary_model(log_dir, force_cloud=test_cloud) as model:
         # await conjureup(model, namespace, bundle, 'stable')
-        await juju_deploy(model, namespace, bundle, charm_channel,
-                          snap_channel)
-        await upgrade_charms(model, upgrade_charm_channel)
-        await upgrade_snaps(model, upgrade_snap_channel)
-        await deploy_e2e(model, upgrade_charm_channel,
-                         upgrade_snap_channel, namespace=namespace)
+        await juju_deploy(model, namespace, bundle, upgrade_from_charm_channel,
+                          upgrade_from_snap_channel)
+        await upgrade_charms(model, test_charm_channel)
+        await upgrade_snaps(model, test_snap_channel)
+        await deploy_e2e(model, test_charm_channel,
+                         test_snap_channel, namespace=namespace)
         await validate_all(model, log_dir)
 
 
 @pytest.mark.asyncio
 async def test_bundletester(log_dir):
-    await run_bundletester(namespace, log_dir, channel=charm_channel,
-                           snap_channel=snap_channel, force_cloud=test_cloud)
+    await run_bundletester(namespace, log_dir, channel=test_charm_channel,
+                           snap_channel=test_snap_channel,
+                           force_cloud=test_cloud)
