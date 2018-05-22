1af02385b2bbe381a59934cfb4df3dd2bb22c362
Kevin W Monroe <kevin.monroe@canonical.com>
2018-05-22 09:26:45 -0500

    treat k8s-test like the others (no special amd64 handling)

--- snaps/build-and-release-aws-eks-snaps.sh
+++ snaps/build-and-release-aws-eks-snaps.sh
@@ -17,15 +17,11 @@ rm -rf ./release
 git clone https://github.com/juju-solutions/release.git --branch rye/snaps --depth 1
 (cd release/snap
   make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH="$KUBE_ARCH" \
-    targets="kubectl kubelet kube-proxy"
-  make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH="amd64" \
-    targets="kubernetes-test"
+    targets="kubectl kubelet kube-proxy kubernetes-test"
 )
 
-for app in kubectl kubelet kube-proxy; do
+for app in kubectl kubelet kube-proxy kubernetes-test; do
   for arch in $KUBE_ARCH; do
     retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_${arch}.snap --release ${MAIN_VERSION}/${EKS_RELEASE}
   done
 done
-
-retry snapcraft push release/snap/build/kubernetes-test_${KUBE_VERSION:1}_amd64.snap --release ${MAIN_VERSION}/${EKS_RELEASE}
