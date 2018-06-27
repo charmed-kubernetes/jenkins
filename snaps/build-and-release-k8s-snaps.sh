6dcacb106a57cff8348f66b46725870498bb9aed
George Kraft <cynerva@gmail.com>
2018-06-27 06:46:31 -0500

    Make build-and-release-k8s-snaps.sh runnable on s390x (#206)

--- snaps/build-and-release-k8s-snaps.sh
+++ snaps/build-and-release-k8s-snaps.sh
@@ -10,7 +10,8 @@
 set -eux
 
 KUBE_VERSION="${KUBE_VERSION:-$(curl -L https://dl.k8s.io/release/stable.txt)}"
-KUBE_ARCH="amd64"
+KUBE_ARCH="$(dpkg --print-architecture)"
+SKIP_RELEASE_TAG="${SKIP_RELEASE_TAG:-false}"
 
 source utilities.sh
 VERSION=$(get_major_minor $KUBE_VERSION)
@@ -22,9 +23,7 @@ rm -rf ./release
 git clone https://github.com/juju-solutions/release.git --branch rye/snaps --depth 1
 (cd release/snap
   make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH="$KUBE_ARCH" \
-    targets="kubeadm kube-apiserver kubectl kubelet kube-proxy kube-scheduler"
-  make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH="amd64" \
-    targets="kube-controller-manager kubernetes-test"
+    targets="kubeadm kube-apiserver kubectl kubelet kube-proxy kube-scheduler kube-controller-manager kubernetes-test"
 )
 
 rm -rf ./cdk-addons
@@ -32,26 +31,20 @@ if git ls-remote --exit-code --heads https://github.com/juju-solutions/cdk-addon
 then
   echo "Getting cdk-addons from ${ADDONS_BRANCH_VERSION} branch."
   git clone https://github.com/juju-solutions/cdk-addons.git --branch ${ADDONS_BRANCH_VERSION} --depth 1
-  tag_release  "juju-solutions" "cdk-addons" ${ADDONS_BRANCH_VERSION} ${GH_USER} ${GH_TOKEN} ${KUBE_VERSION}
+  if [ "$SKIP_RELEASE_TAG" != "true" ]; then
+    tag_release  "juju-solutions" "cdk-addons" ${ADDONS_BRANCH_VERSION} ${GH_USER} ${GH_TOKEN} ${KUBE_VERSION}
+  fi
 else
   echo "Branch for ${ADDONS_BRANCH_VERSION} does not exist. Getting cdk-addons from master head."
   git clone https://github.com/juju-solutions/cdk-addons.git --depth 1
-  tag_release  "juju-solutions" "cdk-addons" "master" ${GH_USER} ${GH_TOKEN} ${KUBE_VERSION}
+  if [ "$SKIP_RELEASE_TAG" != "true" ]; then
+    tag_release  "juju-solutions" "cdk-addons" "master" ${GH_USER} ${GH_TOKEN} ${KUBE_VERSION}
+  fi
 fi
-for arch in $KUBE_ARCH; do
-  (cd cdk-addons && make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH=${arch})
+(cd cdk-addons && make KUBE_VERSION=$KUBE_VERSION KUBE_ARCH=${KUBE_ARCH})
+
+for app in kubeadm kube-apiserver kubectl kubelet kube-proxy kube-scheduler kube-controller-manager kubernetes-test; do
+  retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_${KUBE_ARCH}.snap --release ${VERSION}/edge
 done
 
-for app in kubeadm kube-apiserver kubectl kubelet kube-proxy kube-scheduler; do
-  for arch in $KUBE_ARCH; do
-    retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_${arch}.snap --release ${MAIN_VERSION}/edge
-  done
-done
-
-for app in kube-controller-manager kubernetes-test; do
-  retry snapcraft push release/snap/build/${app}_${KUBE_VERSION:1}_amd64.snap --release ${MAIN_VERSION}/edge
-done
-
-for arch in $KUBE_ARCH; do
-  retry snapcraft push cdk-addons/cdk-addons_${KUBE_VERSION:1}_${arch}.snap --release ${MAIN_VERSION}/edge
-done
+retry snapcraft push cdk-addons/cdk-addons_${KUBE_VERSION:1}_${KUBE_ARCH}.snap --release ${VERSION}/edge
