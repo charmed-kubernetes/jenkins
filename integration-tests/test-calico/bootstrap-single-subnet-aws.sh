c3ad5d0b6892685deeda533742eb370fbc0556c6
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-06-29 19:34:58 +0300

    Addressing comments

--- integration-tests/test-calico/bootstrap-single-subnet-aws.sh
+++ integration-tests/test-calico/bootstrap-single-subnet-aws.sh
@@ -8,11 +8,15 @@ alias aws="aws --output text"
 
 # Pre-deploy: Create a single-subnet VPC
 VPC_ID=$(aws ec2 create-vpc --cidr $VPC_CIDR | cut -f 7)
+aws ec2 create-tags --resource $VPC_ID --tags Key=created-by,Value=test-calico
 SUBNET0_ID=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block $SUBNET0_CIDR --availability-zone us-east-1b | cut -f 9)
+aws ec2 create-tags --resource $SUBNET0_ID --tags Key=created-by,Value=test-calico
 aws ec2 modify-subnet-attribute --subnet-id $SUBNET0_ID --map-public-ip-on-launch
 GATEWAY_ID=$(aws ec2 create-internet-gateway | cut -f 2)
+aws ec2 create-tags --resource $GATEWAY_ID --tags Key=created-by,Value=test-calico
 aws ec2 attach-internet-gateway --vpc-id $VPC_ID --internet-gateway $GATEWAY_ID
 ROUTE_TABLE_ID=$(aws --output text ec2 describe-route-tables | grep $VPC_ID | cut -f 2)
+aws ec2 create-tags --resource $ROUTE_TABLE_ID --tags Key=created-by,Value=test-calico
 aws ec2 create-route --route-table-id $ROUTE_TABLE_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $GATEWAY_ID
 aws ec2 modify-vpc-attribute --vpc-id=$VPC_ID --enable-dns-support
 aws ec2 modify-vpc-attribute --vpc-id=$VPC_ID --enable-dns-hostnames
--- integration-tests/test-calico/test_calico.sh
+++ integration-tests/test-calico/test_calico.sh
@@ -15,7 +15,6 @@ trap cleanup EXIT
 
 # Deploy kubernetes with calico
 juju deploy cs:~containers/kubernetes-calico --channel ${CHARM_CHANNEL}
-#juju deploy cs:~cynerva/bundle/kubernetes-calico-9
 
 # Deploy e2e and an extra worker for it
 juju deploy cs:~containers/kubernetes-e2e --channel ${CHARM_CHANNEL}
@@ -24,10 +23,12 @@ juju relate kubernetes-e2e kubernetes-master:kube-control
 juju relate kubernetes-e2e kubernetes-master:kube-api-endpoint
 
 # Disable source-dest-check on all instances
-for machine in 0 1 2 3 4 5 6 7 8 9; do
+MACHINES_CSV=`juju status --format json | jq -r '.machines | keys | @csv' | tr -d \"`
+IFS=$','
+for machine in $MACHINES_CSV; do
   until juju status --format yaml $machine | grep instance-id | grep -v pending; do sleep 10; done
   INSTANCE_ID=$(juju status --format yaml $machine | grep instance-id | head -n 1 | cut -d " " -f 6)
   aws ec2 modify-instance-attribute --instance-id $INSTANCE_ID --source-dest-check '{"Value": false}'
 done
 
-pytest -s ./integration-tests/test_live_model.py
+pytest -s --no-print-logs ./integration-tests/test_live_model.py
