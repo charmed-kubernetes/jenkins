99e34c88f8eb9ea34f1bfe824393643c6eace2ce
Mike Wilson <knobby@burntsheep.com>
2018-07-11 15:37:08 -0400

    Adding script to keep our kubernetes fork up to date

---
+++ maintenance/update-juju-solutions-fork.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script will update juju-solutions/kubernetes with content from kubernetes. It will only perform a fast-forward 
+# merge, which should work for us since we should be the only one changing charm code.
+# In the rare case that the merge fails, we can manually merge. The Jenkins job will notify via IRC when it fails.
+
+git checkout master
+if ! git remote|grep upstream; then
+  git remote add upstream https://github.com/kubernetes/kubernetes.git
+fi
+git pull upstream master
+git merge upstream/master
+git push https://${CDKBOT_LOGIN}@github.com/juju-solutions/kubernetes master
