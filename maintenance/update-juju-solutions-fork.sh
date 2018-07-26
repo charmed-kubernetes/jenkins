1d452d980ead58c0bc24195aaba5e9f420455186
George Kraft <cynerva@gmail.com>
2018-07-26 08:17:44 -0500

    Fix "Pull from upstream kubernetes" job not keeping up with fork (#224)

--- maintenance/update-juju-solutions-fork.sh
+++ maintenance/update-juju-solutions-fork.sh
@@ -6,9 +6,9 @@ set -eu
 # In the rare case that the merge fails, we can manually merge. The Jenkins job will notify via IRC when it fails.
 
 git checkout master
+git pull origin master
 if ! git remote|grep upstream; then
   git remote add upstream https://github.com/kubernetes/kubernetes.git
 fi
 git pull upstream master
-git merge upstream/master
 git push https://${CDKBOT_LOGIN}@github.com/juju-solutions/kubernetes master
