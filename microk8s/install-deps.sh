e28355fb972861b00d84f6e3058487dd6bdc7532
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-09-14 16:35:33 +0300

    Microk8s release to beta and stable workflows

--- microk8s/install-deps.sh
+++ microk8s/install-deps.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 # known good version 1.10.6
-sudo pip3 install -U launchpadlib
-# Known good version 0.4.4
-sudo pip3 install -U  canonicalwebteam.snapstoreapi
+sudo pip3 install -U launchpadlib python-dateutil
+sudo snap install snapcraft --classic
