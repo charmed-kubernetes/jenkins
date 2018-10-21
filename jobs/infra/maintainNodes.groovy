0d685c58b8c9cfeeb37f36e29cc5ef8539c5ee85
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-21 12:56:27 -0400

    update maintain
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/maintainNodes.groovy
+++ jobs/infra/maintainNodes.groovy
@@ -1,9 +1,9 @@
 @Library('juju-pipeline@master') _
 
+def nodes = ['runner-amd64', 'runner-amd64-2', 'runner-s390x', 'runner-arm64', 'runner-ppc64le']
+
 pipeline {
-    agent {
-        label 'runner'
-    }
+    agent none
     // Add environment credentials for pyjenkins script on configuring nodes automagically
     environment {
         PATH = "/snap/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin"
@@ -15,10 +15,17 @@ pipeline {
     }
     stages {
         stage('Configure systems') {
-            steps {
-                installTools()
-                dir("jobs/infra") {
-                    sh "/usr/local/bin/pipenv run ansible-playbook playbook-jenkins.yml -e 'ansible_python_interpreter=python3'"
+            script {
+                nodes.each { node ->
+                    agent {
+                        label node
+                    }
+                    steps {
+                        installToolsJenkaas()
+                        // dir("jobs/infra") {
+                        //     sh "/usr/local/bin/pipenv run ansible-playbook playbook-jenkins.yml -e 'ansible_python_interpreter=python3'"
+                        // }
+                    }
                 }
             }
         }
