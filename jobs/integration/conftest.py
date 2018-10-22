79c40868bd1fe67af525ae49616991140dfe008d
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-22 12:15:34 -0400

    passenv
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/integration/conftest.py
+++ jobs/integration/conftest.py
@@ -48,7 +48,6 @@ async def deploy():
     _model = '{}-{}'.format(MODEL,
                             test_run_nonce)
 
-    print(os.getenv('HOME'))
     juju('add-model', '-c', CONTROLLER, _model)
     juju('model-config', '-m',
          '{}:{}'.format(CONTROLLER, _model), 'test-mode=true')
