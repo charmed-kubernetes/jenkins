61272efcf4f4f1938b939b1663ca3db4b0da9ff7
Adam Stokes <battlemidget@users.noreply.github.com>
2018-11-14 14:12:16 -0500

    test lxc
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/execWorkspace.groovy
+++ jobs/infra/execWorkspace.groovy
@@ -17,14 +17,22 @@ pipeline {
         stage('Running') {
             steps {
                 // sh "cd jobs && tox -e py36 -- aws s3 --profile s3 ls s3://jujubigdata"
-                script {
-                    if (params.workspace_path) {
-                        sh "sudo rm -rf ${params.workspace_path}"
-                    }
-                    if (params.exec_command) {
-                        sh "${params.exec_command}"
-                    }
+                retry(10){
+                    sh "sudo lxc launch ubuntu:16.04 piptest"
+                    sh "sudo lxc exec piptest -- /bin/bash -c 'apt install -qyf python3-pip"
+                    sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install requests"
+                    sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install sh"
+                    sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install launchpadlib"
+                    sh "sudo lxc delete --force piptest"
                 }
+                // script {
+                //     if (params.workspace_path) {
+                //         sh "sudo rm -rf ${params.workspace_path}"
+                //     }
+                //     if (params.exec_command) {
+                //         sh "${params.exec_command}"
+                //     }
+                // }
             }
         }
     }
