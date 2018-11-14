28bab99e9b6e784f43c18a50efec9377f4d46cdf
Adam Stokes <battlemidget@users.noreply.github.com>
2018-11-14 15:55:47 -0500

    loop
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/execWorkspace.groovy
+++ jobs/infra/execWorkspace.groovy
@@ -21,15 +21,17 @@ pipeline {
                 sh "which lxc"
                 sh "sudo lxc delete --force piptest || true"
                 sh "sudo lxc profile show default"
-                retry(10){
-                    sh "sudo lxc launch ubuntu:16.04 piptest"
-                    sh "sleep 45"
-                    sh "sudo lxc exec piptest -- /bin/bash -c 'apt-get update' || true"
-                    sh "sudo lxc exec piptest -- /bin/bash -c 'apt-get install -qyf python3-pip' || true"
-                    sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install requests' || true"
-                    sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install sh' || true"
-                    sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install launchpadlib' || true"
-                    sh "sudo lxc delete --force piptest"
+                script {
+                    for (int i = 0; i < 15; i++) {
+                        sh "sudo lxc launch ubuntu:16.04 piptest"
+                        sh "sleep 45"
+                        sh "sudo lxc exec piptest -- /bin/bash -c 'apt-get update' || true"
+                        sh "sudo lxc exec piptest -- /bin/bash -c 'apt-get install -qyf python3-pip' || true"
+                        sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install requests' || true"
+                        sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install sh' || true"
+                        sh "sudo lxc exec piptest -- /bin/bash -c 'pip3 install launchpadlib' || true"
+                        sh "sudo lxc delete --force piptest"
+                    }
                 }
                 // script {
                 //     if (params.workspace_path) {
