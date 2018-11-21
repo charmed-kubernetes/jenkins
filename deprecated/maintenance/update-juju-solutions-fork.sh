f689120383e7f0243a03f217a2548a6e0e706565
Adam Stokes <battlemidget@users.noreply.github.com>
2018-11-20 21:22:05 -0500

    really move maintenance
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ deprecated/maintenance/update-juju-solutions-fork.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+# This script will update juju-solutions/kubernetes with content from kubernetes. It will attempt a merge
+# which should work for us since we should be the only one changing charm code.
+# In the rare case that the merge fails, we can manually merge. The Jenkins job will notify via IRC when it fails.
+
+git checkout master
+git pull origin master
+if ! git remote|grep upstream; then
+  git remote add upstream https://github.com/kubernetes/kubernetes.git
+fi
+git pull upstream master
+git push https://${CDKBOT_LOGIN}@github.com/juju-solutions/kubernetes master
