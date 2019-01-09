97f73ba4dec71fc537aabfaaa46e1ad3a9258c26
Adam Stokes <battlemidget@users.noreply.github.com>
2019-01-09 12:50:31 -0500

    update properties
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/build-snaps/build-release-eks.groovy
+++ jobs/build-snaps/build-release-eks.groovy
@@ -30,7 +30,7 @@ pipeline {
                     script {
                         sh "${snap_sh} build --arch amd64 ${eks_snaps} --version ${version} --match-re \'(?=\\S*[-]*)([a-zA-Z-]+)(.*)\' --rename-re \'\\1-eks'"
                         sh "sudo chown jenkins:jenkins -R release/snap"
-                        sh "${snap_sh} release --channels ${channel}"
+                        sh "${snap_sh} release --channels ${params.channels}"
                     }
                 }
             }
