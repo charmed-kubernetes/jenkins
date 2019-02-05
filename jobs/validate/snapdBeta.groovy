20982086ba4f5d683a85ae4686dd774777b585b0
Adam Stokes <battlemidget@users.noreply.github.com>
2019-02-05 09:44:50 -0500

    Add job for running weekly validate test against snap core beta
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ jobs/validate/snapdBeta.groovy
@@ -0,0 +1,30 @@
+@Library('juju-pipeline@master') _
+
+def juju_model = String.format("%s-%s", params.model, uuid())
+
+pipeline {
+    agent {
+        label 'runner-amd64'
+    }
+    /* XXX: Global $PATH setting doesn't translate properly in pipelines
+     https://stackoverflow.com/questions/43987005/jenkins-does-not-recognize-command-sh
+     */
+    environment {
+        PATH = "/snap/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin"
+    }
+    options {
+        ansiColor('xterm')
+        timestamps()
+    }
+    stages {
+        stage('Run k8s-core job') {
+            options {
+                timeout(time: 2, unit: 'HOURS')
+            }
+            steps {
+                build job:"validate-v1.14.x-kubernetes-core",
+                    parameters: [booleanParam(name:'with_beta_snapd', value: true)]
+            }
+        }
+    }
+}
