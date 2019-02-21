fee0b4b5cf3454d61eebda3d255359536c109fc3
Adam Stokes <battlemidget@users.noreply.github.com>
2019-02-21 10:09:39 -0500

    try another format
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/build-snaps/snaps-source.py
+++ jobs/build-snaps/snaps-source.py
@@ -145,7 +145,7 @@ def create_snap_recipe(
                                         password=snap_recipe_password)
     discharge_macaroon = cip.get_discharge(caveat_id)
     snap_recipe.completeAuthorization(
-        discharge_macaroon=discharge_macaroon.json())
+        discharge_macaroon=discharge_macaroon.text)
     snap_recipe.requestBuilds(archive=_client.archive(), pocket='Updates')
 
 
