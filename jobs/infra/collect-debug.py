7303e5dcfd275be75d9a57486f1cee2dfcb6e2d0
Adam Stokes <battlemidget@users.noreply.github.com>
2019-02-26 14:37:03 +0000

    change path to k8sci
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/collect-debug.py
+++ jobs/infra/collect-debug.py
@@ -24,7 +24,7 @@ def results(bucket, results_file):
     results_file = Path(results_file)
     current_date = datetime.now().strftime('%Y-%m-%d')
     env = os.environ.copy()
-    s3_path = f"s3://{bucket}/{env['JOB_NAME']}/{current_date}/{env['BUILD_NUMBER']}/{results_file.parent}"
+    s3_path = f"s3://{bucket}/k8sci/{env['JOB_NAME']}/{current_date}/{env['BUILD_NUMBER']}/{results_file.parent}"
     click.echo(s3_path)
     click.echo(s3.cp(str(results_file), s3_path))
     s3.cp(str(results_file), s3_path)
