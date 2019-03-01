7971a2faf36b95dbbb91abefc85668ed75bffd4e
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2019-03-01 12:18:20 +0100

    Do not touch 1.14+ releases until we automate the build process of pre-stable releases

--- jobs/microk8s/configbag.py
+++ jobs/microk8s/configbag.py
@@ -23,9 +23,9 @@ def get_tracks(all=False):
     '''
     arch = get_arch()
     if arch == 'arm64' and not all:
-        return ["latest", "1.12", "1.13", "1.14", "1.15"]
+        return ["latest", "1.12", "1.13"]
     else:
-        return ["latest", "1.10", "1.11", "1.12", "1.13", "1.14", "1.15"]
+        return ["latest", "1.10", "1.11", "1.12", "1.13"]
 
 
 snap_name = "microk8s"
