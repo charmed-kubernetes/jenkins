bb7674a707f93a96ea362340457a16199c0a6ca3
Adam Stokes <battlemidget@users.noreply.github.com>
2019-03-29 18:42:19 -0400

    finish tracker
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/release/release-tracker.py
+++ jobs/release/release-tracker.py
@@ -1,61 +1,66 @@
 """ Tracks the progress of a release through it's different stages
 """
 
-import click
+import argparse
 import boto3
-from kv import KV
+import sys
 
-db = KV('tracker.db')
-session = boto3.Session(region_name='us-east-1')
-dynamodb = session.resource('dynamodb')
-table = dynamodb.Table('ReleaseTracker')
+def parse_options(argv):
+    parser = argparse.ArgumentParser(prog="release-tracker")
+    parser.add_argument('-d', '--debug', action='store_true',
+                        dest='debug', default=False,
+                        help='Enable debug logging.')
+    parser.add_argument('--release-id', dest='release_id',
+                        help='Release ID of job')
 
-@click.group()
-def cli():
-    pass
+    subparsers = parser.add_subparsers()
+    phase = subparsers.add_parser('set-phase')
+    phase.add_argument('--name', dest='phase_name')
+    phase.add_argument(
+        '--result', dest='phase_result',
+        type=str,
+        choices=['pass', 'fail', 'timeout'],
+        help='result of phase')
+    phase.set_defaults(func=set_phase)
 
+    g_phase = subparsers.add_parser('get-phase')
+    g_phase.add_argument('--name', dest='phase_name')
+    g_phase.set_defaults(func=get_phase)
 
-@cli.command()
-def store_results(release_id):
+    return parser.parse_args(argv)
+
+def store_results(db):
     """ saves the current state of release
     """
-    tracker_db['release_id'] = release_id
-    # try to pull existing release id data
-    response = table.get_item(
-        Key={'release_id': release_id}
-    )
-    if response and 'Item' in response:
-        tracker_db = response['Item']
+    table.put_item(Item=dict(db))
 
-
-    table.put_item(Item=dict(tracker_db))
-
-@cli.command()
-@click.option('--release-id', required=True, help="release_id of release job")
-@click.argument('phase')
-def get_phase(release_id, phase):
+def get_phase(db, opts):
     """ checks for existing phase and returns result
     """
-    response = table.get_item(
-        Key={'release_id': release_id}
-    )
-    if response and 'Item' in response:
-        return int(response['Item'][phase])
-    return 1
+    print(db.get(opts.phase_name, 'fail'))
 
-@cli.command()
-@click.option('--release-id', required=True, help="release_id of release job")
-@click.argument('phase')
-@click.argument('result')
-def set_phase(release_id, phase, result):
+def set_phase(db, opts):
     """ sets a phase result
 
     0 for pass, 1 for fail, 2 for timeout
     """
-    table.put_item(
-        Item=dict(db)
-    )
+    db[opts.phase_name] = opts.phase_result
+    store_results(db)
 
 if __name__ == "__main__":
-    cli()
+    db = {}
+    session = boto3.Session(region_name='us-east-1')
+    dynamodb = session.resource('dynamodb')
+    table = dynamodb.Table('ReleaseTracker')
 
+    opts = parse_options(sys.argv[1:])
+
+    response = table.get_item(
+        Key={'release_id': opts.release_id}
+    )
+    if response and 'Item' in response:
+        db = response['Item']
+    else:
+        db['release_id'] = opts.release_id
+
+    opts.func(db, opts)
