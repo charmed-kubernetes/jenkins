5cdeddb849c05970eed5160587ec4f579aebb537
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2019-04-01 15:03:31 +0300

    Re-add automation for 1.14, 1.15

--- jobs/microk8s/configbag.py
+++ jobs/microk8s/configbag.py
@@ -23,9 +23,9 @@ def get_tracks(all=False):
     '''
     arch = get_arch()
     if arch == 'arm64' and not all:
-        return ["latest", "1.12", "1.13"]
+        return ["latest", "1.12", "1.13", "1.14", "1.15"]
     else:
-        return ["latest", "1.10", "1.11", "1.12", "1.13"]
+        return ["latest", "1.10", "1.11", "1.12", "1.13", "1.14", "1.15"]
 
 
 snap_name = "microk8s"
