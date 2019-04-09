94ab86ddb183dd9683d8cef35596b34b70fe9618
Adam Stokes <battlemidget@users.noreply.github.com>
2019-04-09 11:28:59 -0400

    Update integrator test, spec out setup/teardown routine
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/integration/test_integrator_charm.py
+++ jobs/integration/test_integrator_charm.py
@@ -17,37 +17,44 @@ def template_path():
     return here / 'templates/integrator-charm-data' / CLOUD
 
 
-@pytest.mark.asyncio
-def test_create_storage():
-    """ Test creating a storage class
+@pytest.fixture(scope='function')
+def setup_storage_elb_resource(request):
+    """ Sets up and tearsdown k8s resources
     """
-    kubectl_yml = template_path() / 'storage-class.yaml'
-    sh.kubectl.create(f=kubectl_yml)
-    pass
+    def setup_storage_elb_resource_teardown():
+        print("Perform teardown of resources")
 
-@pytest.mark.asyncio
-def test_pv_claim():
-    """ Test creating a persistent volume claim
-    """
-    kubectl_yml = template_path() / 'pv-claim.yaml'
-    sh.kubectl.create(f=kubectl_yml)
-    pass
+    storage_yml = template_path() / 'storage-class.yaml'
+    sh.kubectl.create(f=str(storage_yml))
 
-@pytest.mark.asyncio
-def test_bbox():
-    """ Test creating a bbox pod
-    """
-    kubectl_yml = template_path() / 'bbox.yaml'
-    sh.kubectl.create(f=kubectl_yml)
-    pass
+    pv_claim_yml = template_path() / 'pv-claim.yaml'
+    sh.kubectl.create(f=str(pv_claim_yml))
+
+    # bbox_yml = template_path() / 'bbox.yaml'
+    # sh.kubectl.create(f=str(bbox_yml))
 
 
 @pytest.mark.asyncio
-def test_load_balancer():
+def test_load_balancer(setup_storage_elb_resource):
     """ Performs a deployment of hello-world with newly created LB and attempts
     to do a requests.get and parse the html to verify the lb ip address is
     functioning appropriately.
     """
-    # kubectl run hello-world --replicas=5 --labels="run=load-balancer-example" --image=gcr.io/google-samples/node-hello:1.0  --port=8080
-    # kubectl expose deployment hello-world --type=LoadBalancer --name=hello
-    pass
+    sh.kubectl.run("hello-world",
+                   replicas=5,
+                   labels='run=load-balancer-example',
+                   image="gcr.io/google-samples/node-hello:1.0",
+                   port=8080)
+    sh.kubectl.expose("deployment",
+                      "hello-world",
+                      type="LoadBalancer",
+                      name="hello")
+    out = sh.kubectl.get('svc', o='yaml', selector='run=load-balancer-example')
+    svc = yaml.safe_load(out.stdout.decode('utf8'))
+    try:
+        lb_ip = svc['status']['loadBalancer']['ingress'][0]
+        set_url = f"{lp_ip}:8080"
+        html = await asyncify(requests.get)(set_url)
+        assert 'Hello Kubernetes!' in html.content
+    except:
+        print("Failed to get content, needs a timeout?")
