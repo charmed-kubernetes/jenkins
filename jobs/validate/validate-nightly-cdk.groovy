8c760b8377eda8a5b85a4f61d0747289da14b2e8
Adam Stokes <battlemidget@users.noreply.github.com>
2019-05-01 08:37:14 -0400

    Add addons to nightly midnight job runs
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/validate/validate-nightly-cdk.groovy
+++ jobs/validate/validate-nightly-cdk.groovy
@@ -12,7 +12,7 @@ pipeline {
         timestamps()
     }
     stages {
-        stage('Validate') {
+        stage('Validate CDK') {
             steps {
                 script {
                     def jobs = [:]
@@ -31,5 +31,82 @@ pipeline {
                 }
             }
         }
+        stage('Validate NVidia') {
+            steps {
+                script {
+                    def jobs = [:]
+                    def releases = readYaml file: 'jobs/includes/k8s-support-matrix.inc'
+                    releases.each { k ->
+                        def release = k.keySet().first()
+                        def options = k.values()
+                        jobs[release] = {
+                            stage("Validate NVidia: ${options.normalized_ver}") {
+                                build job:"validate-nvidia-${release}",
+                                    parameters: [string(name:'cloud', value: 'aws/us-east-1')]
+                            }
+                        }
+                    }
+                    parallel jobs
+                }
+            }
+        }
+        stage('Validate Calico') {
+            steps {
+                script {
+                    def jobs = [:]
+                    def releases = readYaml file: 'jobs/includes/k8s-support-matrix.inc'
+                    releases.each { k ->
+                        def release = k.keySet().first()
+                        def options = k.values()
+                        jobs[release] = {
+                            stage("Validate Calico: ${options.normalized_ver}") {
+                                build job:"validate-calico-${release}",
+                                    parameters: [string(name:'cloud', value: 'aws/us-east-1')]
+                            }
+                        }
+                    }
+                    parallel jobs
+                }
+            }
+        }
+        stage('Validate Vault') {
+            steps {
+                script {
+                    def jobs = [:]
+                    def releases = readYaml file: 'jobs/includes/k8s-support-matrix.inc'
+                    releases.each { k ->
+                        def release = k.keySet().first()
+                        def options = k.values()
+                        jobs[release] = {
+                            stage("Validate Vault: ${options.normalized_ver}") {
+                                build job:"validate-vault-${release}",
+                                    parameters: [string(name:'cloud', value: 'aws/us-east-1')]
+                            }
+                        }
+                    }
+                    parallel jobs
+                }
+            }
+        }
+        stage('Validate Tigera EE') {
+            steps {
+                script {
+                    def jobs = [:]
+                    def releases = readYaml file: 'jobs/includes/k8s-support-matrix.inc'
+                    releases.each { k ->
+                        def release = k.keySet().first()
+                        def options = k.values()
+                        jobs[release] = {
+                            stage("Validate Tigera EE: ${options.normalized_ver}") {
+                                build job:"validate-tigera-secure-ee-${release}",
+                                    parameters: [string(name:'cloud', value: 'aws/us-east-1')]
+                            }
+                        }
+                    }
+                    parallel jobs
+                }
+            }
+        }
+
     }
 }
