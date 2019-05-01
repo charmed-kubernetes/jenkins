f619bac9bd400a9d01ca3c94a53a35db3d6158d9
George Kraft <cynerva@gmail.com>
2019-05-01 15:22:17 -0500

    Add validate-calico-upgrade jobs

--- jobs/integration/test_cdk.py
+++ jobs/integration/test_cdk.py
@@ -4,14 +4,8 @@ from .utils import upgrade_charms, upgrade_snaps
 from .validation import validate_all
 from .base import UseModel
 
-namespace = os.environ.get('TEST_CHARM_NAMESPACE', 'containers')
 test_charm_channel = os.environ.get('TEST_CHARM_CHANNEL', 'edge')
-test_snap_channel = os.environ.get('TEST_SNAP_CHANNEL', 'edge')
-test_cloud = os.environ.get('TEST_CLOUD', '')
-upgrade_from_snap_channel = os.environ.get(
-    'UPGRADE_FROM_SNAP_CHANNEL', 'stable')
-upgrade_from_charm_channel = os.environ.get(
-    'UPGRADE_FROM_CHARM_CHANNEL', 'stable')
+test_snap_channel = os.environ.get('TEST_SNAP_CHANNEL')
 
 
 @pytest.mark.asyncio
@@ -26,5 +20,6 @@ async def test_validate(log_dir):
 async def test_upgrade(log_dir):
     async with UseModel() as model:
         await upgrade_charms(model, test_charm_channel)
-        await upgrade_snaps(model, test_snap_channel)
+        if test_snap_channel:
+            await upgrade_snaps(model, test_snap_channel)
         await validate_all(model, log_dir)
