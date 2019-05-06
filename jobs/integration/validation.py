96b47ac49d9ae38adc54b08e447f6e20a9f8cc65
Mike Wilson <knobby@burntsheep.com>
2019-05-06 13:57:48 -0400

    Adding test for docker-opts
    prevents regression of https://github.com/juju-solutions/layer-docker/pull/136

--- jobs/integration/validation.py
+++ jobs/integration/validation.py
@@ -62,6 +62,7 @@ async def validate_all(model, log_dir):
             cpu_arch not in ['s390x', 'arm64', 'aarch64']):
         await validate_encryption_at_rest(model)
     await validate_dns_provider(model)
+    await validate_docker_opts(model)
 
 
 @log_calls_async
@@ -1305,6 +1306,39 @@ async def validate_dns_provider(model):
 
     # Cleanup
     await cleanup()
+
+
+async def validate_docker_opts(model):
+    worker_app = model.applications['kubernetes-worker']
+
+    async def verify_default_docker(worker, opt):
+        action = await worker.run('cat /etc/default/docker')
+        return opt in action.results['Stdout']
+
+    async def verify_ps_output(worker, opt):
+        action = await worker.run('ps -aux|grep dockerd')
+        return opt in action.results['Stdout']
+
+    log('validating dockerd options')
+
+    for opt_to_test in ['--experimental', '--insecure-registry 10.0.0.1:5000']:
+        await worker_app.set_config({'docker-opts': opt_to_test})
+        log('set dockerd config ' + opt_to_test)
+
+        for worker in worker_app.units:
+            log('verifying worker ' + worker.entity_id)
+            log(' - dockerd config')
+            await retry_async_with_timeout(verify_default_docker,
+                                           (worker, opt_to_test),
+                                           timeout_msg="docker opt test did not pass",
+                                           timeout_insec=120)
+            log(' - ps output')
+            await retry_async_with_timeout(verify_ps_output,
+                                           (worker, opt_to_test),
+                                           timeout_msg="docker ps test did not pass",
+                                           timeout_insec=120)
+
+    await worker_app.set_config({'docker-opts': ""})
 
 
 class MicrobotError(Exception):
