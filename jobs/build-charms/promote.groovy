bc4178d27064d1333c154912599d4a5ca16c63b2
Adam Stokes <battlemidget@users.noreply.github.com>
2019-05-23 09:32:01 -0400

    only run charm cmd on amd64
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/build-charms/promote.groovy
+++ jobs/build-charms/promote.groovy
@@ -4,7 +4,7 @@ def to_channels = params.to_channel.split()
 def charm_sh = "${utils.cipy} build-charms/charms.py"
 
 pipeline {
-    agent { label 'runner' }
+    agent { label 'runner-amd64' }
     /* XXX: Global $PATH setting doesn't translate properly in pipelines
      https://stackoverflow.com/questions/43987005/jenkins-does-not-recognize-command-sh
      */
