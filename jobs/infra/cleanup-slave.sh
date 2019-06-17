517371c4129e6a11c59b7c592e1ce5a50300c727
Adam Stokes <battlemidget@users.noreply.github.com>
2019-06-17 13:22:30 -0400

    update cleanup script
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/cleanup-slave.sh
+++ jobs/infra/cleanup-slave.sh
@@ -18,4 +18,19 @@ sudo du -h --max-depth=1 /var/lib
 echo "####### FS Usage (Finish)"
 df -h
 
-lxc query /1.0/storage-pools | jq -r '. | map(. | sub("/1.0/storage-pools/"; "")) | join("\n")' | grep -v default
+
+echo "####### Removing LXD cruft"
+for i in `sudo lxc query /1.0/containers | jq -r '. | map(. | sub("/1.0/containers/"; "")) | join("\n")'`
+do
+    sudo lxc delete --force $i
+done
+
+for i in `sudo lxc query /1.0/storage-pools | jq -r '. | map(. | sub("/1.0/storage-pools/"; "")) | join("\n")' | grep -v default`
+do
+    for x in `sudo lxc query /1.0/storage-pools/$i/volumes |jq -r '. | map(. | sub("/1.0/storage-pools/$i/volumes/container/"; "") | sub("/1.0/storage-pools/$i/volumes/image/"; "")) | join("\n")' | grep -v default`
+    do
+        sudo lxc storage volume delete $i $x
+    done
+    sudo lxc storage delete $i
+
+done
