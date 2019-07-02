282ae1df98db6638a8262e419f1468b93f11da6b
Peter J De Sousa <peter.de.sousa@canonical.com>
2019-07-02 09:10:49 +0100

    Jenkins tests to test https-envs
    Added tests to check layer-docker, charm-docker, and
    charm-containerd for correct setting of HTTP(S)
    environment variables.

--- jobs/integration/test_juju_https_proxy_legacy.py
+++ jobs/integration/test_juju_https_proxy_legacy.py
@@ -30,11 +30,11 @@ async def setup_proxy(model):
 
 
 @log_calls_async
-async def test_kube_node_conf(worker_unit, runtime):
+async def test_kube_node_conf(worker_unit):
     container_runtime_conf_worker = ""
     with NamedTemporaryFile() as fp:
         await worker_unit.scp_from(
-            "/lib/systemd/system/%s.service" % runtime,
+            "/lib/systemd/system/docker.service",
             fp.name
         )
         with open_file(fp.name, 'r') as stream:
@@ -55,29 +55,22 @@ async def test_kube_node_conf(worker_unit, runtime):
 
 
 @log_calls_async
-async def test_http_conf_existing_container_runtime(model, runtime):
-
-    container_endpoint = "%s:%s" % (runtime, runtime)
-
-    log('Adding container runtime to the model')
-    container_runtime_env = "%s_RUNTIME_VERSION" % runtime.upper()
-    container_runtime = await model.deploy('cs:~containers/%s-%s' % (runtime, os.environ[container_runtime_env]), num_units=0)
-    await model.add_relation(container_endpoint, 'kubernetes-master:container-runtime')
-    await model.add_relation(container_endpoint, 'kubernetes-worker:container-runtime')
-
+async def test_http_conf_existing_container_runtime(model):
 
     log('waiting...')
     await asyncify(_juju_wait)()
     log('Setting up proxy.')
     proxy_app = await setup_proxy(model)
     proxy = proxy_app.units[0]
-
+    kubernetes_worker_app = model.applications['kubernetes-worker']
+    kubernetes_master_app = model.applications['kubernetes-master']
     # Container runtime config should be overriden by the juju-envs.
     # If this config remains the below regex will fail.
     log('Setting proxy configuration on juju-model.')
     await model.set_config({ 'juju-http-proxy': "http://%s:3128" % proxy.public_address })
     await model.set_config({ 'juju-https-proxy': "https://%s:3128" % proxy.public_address })
-    await container_runtime.set_config({'http_proxy': 'blah'})
+    await kubernetes_worker_app.set_config({'http_proxy': 'blah'})
+    await kubernetes_master_app.set_config({'http_proxy': 'blah'})
     time.sleep(20)
 
 ########## Try this out. The charm should by default be OK to use.
@@ -96,18 +89,17 @@ async def test_http_conf_existing_container_runtime(model, runtime):
 #         await proxy.scp_to(squid_conf.name, '/etc/squid/forwardproxy.conf')
 #     await proxy.ssh("sudo service squid restart")
 
+
     log('waiting...')
     await asyncify(_juju_wait)()
 
     for worker_unit in model.applications['kubernetes-worker'].units:
-        await test_kube_node_conf(worker_unit, runtime)
+        await test_kube_node_conf(worker_unit)
     for worker_unit in model.applications['kubernetes-master'].units:
-        await test_kube_node_conf(worker_unit, runtime)
+        await test_kube_node_conf(worker_unit)
 
     # # Cleanup
     # await controller.destroy_model()
-    await container_runtime.remove_relation(container_endpoint, "kubernetes-worker:container-runtime")
-    await container_runtime.remove_relation(container_endpoint, "kubernetes-master:container-runtime")
     await proxy.remove()
 
     log('waiting...')
@@ -120,6 +112,5 @@ async def test_juju_proxy_vars(log_dir):
     cloud = await controller.get_cloud()
     if cloud is not 'localhost':
         async with UseModel() as model:
-            for container_runtime in ['docker', 'containerd']:
-                await test_http_conf_existing_container_runtime(model, container_runtime)
+            await test_http_conf_existing_container_runtime(model)
     await controller.destroy_model(model.info.uuid)
