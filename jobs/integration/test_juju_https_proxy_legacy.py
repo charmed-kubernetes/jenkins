610c57613bcc26be8835793a2e7c893ee311d44b
Peter J De Sousa <peter.de.sousa@canonical.com>
2019-07-03 10:19:55 +0100

    Refactored proxy
    More refractoring, moved proxy out
    to resolve crashing issues. Tests should be
    good to go.

--- jobs/integration/test_juju_https_proxy_legacy.py
+++ jobs/integration/test_juju_https_proxy_legacy.py
@@ -19,91 +19,126 @@ from juju.controller import Controller
 from builtins import open as open_file
 from tempfile import NamedTemporaryFile
 
-async def setup_proxy(model):
-    log('Adding proxy to the model')
-    proxy_app = await model.deploy("cs:~pjds/squid-forwardproxy-testing-1")
-    log('waiting...')
-    await asyncify(_juju_wait)()
-    proxy_app = model.applications['squid-forwardproxy']
-
-    return proxy_app
 
 
 @log_calls_async
-async def test_kube_node_conf(worker_unit):
-    container_runtime_conf_worker = ""
+async def setup_proxy(model):
+    log('Adding proxy to the model')
+    proxy_app = await model.deploy("cs:~pjds/squid-forwardproxy-testing-1")
+    return proxy_app
+
+
+def get_config_for_rt(runtime):
+    return "/lib/systemd/system/docker.service" \
+        if runtime.lower() == 'docker' \
+        else "/etc/systemd/system/containerd.service.d/proxy.conf"
+
+
+async def get_config_contents(file, unit):
+    conf_contents = ""
     with NamedTemporaryFile() as fp:
-        await worker_unit.scp_from(
-            "/lib/systemd/system/docker.service",
+        await unit.scp_from(
+            file,
             fp.name
         )
         with open_file(fp.name, 'r') as stream:
-            container_runtime_conf_worker = stream.read()
+            conf_contents = stream.read()
+    return conf_contents
 
 
-    log("Configuration file value: %s" % container_runtime_conf_worker)
+@log_calls_async
+async def get_contents(runtime, worker_unit):
+    runtime_conf_contents = ""
+    service_file = get_config_for_rt(runtime)
+    log("Catting service file: %s" % runtime)
+    runtime_conf_contents = await get_config_contents(
+        service_file,
+        worker_unit
+    )
 
+    log("Configuration file value: %s" % runtime_conf_contents)
+    return runtime_conf_contents
+
+CONFIG_REGEX = r"Environment=\"HTTP(S){0,1}_PROXY=([a-zA-Z]{4,5}://[0-9a-zA-Z.]*(:[0-9]{0,5}){0,1}){1,1}\""
+
+
+@log_calls_async
+async def test_kube_node_conf(worker_unit, runtime='docker'):
+    configuration_contents = await get_contents(runtime, worker_unit)
     # Assert runtime config vals were overriden
-    assert 'blah' not in container_runtime_conf_worker
+    assert 'blah' not in configuration_contents
 
     # Assert http value was set
     match = re.search(
-        "Environment=\"HTTP(S){0,1}_PROXY=([a-zA-Z]{4,5}:\/\/[0-9a-zA-Z.]*:[0-9]{0,5}){0,1}\"",
-        container_runtime_conf_worker
+        CONFIG_REGEX,
+        configuration_contents
     )
     assert match is not None
 
 
 @log_calls_async
-async def test_http_conf_existing_container_runtime(model):
+async def test_kube_node_conf_missing(worker_unit, runtime='docker'):
+    configuration_contents = await get_contents(runtime, worker_unit)
+    # Assert runtime config vals were overriden
+    assert 'bla2h' in configuration_contents
 
-    log('waiting...')
-    await asyncify(_juju_wait)()
-    log('Setting up proxy.')
-    proxy_app = await setup_proxy(model)
+    # Assert http value was set
+    match = re.search(
+        CONFIG_REGEX,
+        configuration_contents
+    )
+    assert match is None
+
+
+@log_calls_async
+async def test_http_conf_existing_container_runtime(model, proxy_app):
     proxy = proxy_app.units[0]
-    kubernetes_worker_app = model.applications['kubernetes-worker']
-    kubernetes_master_app = model.applications['kubernetes-master']
+
     # Container runtime config should be overriden by the juju-envs.
     # If this config remains the below regex will fail.
     log('Setting proxy configuration on juju-model.')
-    await model.set_config({ 'juju-http-proxy': "http://%s:3128" % proxy.public_address })
-    await model.set_config({ 'juju-https-proxy': "https://%s:3128" % proxy.public_address })
-    await kubernetes_worker_app.set_config({'http_proxy': 'blah'})
-    await kubernetes_master_app.set_config({'http_proxy': 'blah'})
+    await model.set_config({
+        'juju-http-proxy': "http://%s:3128"
+        % proxy.public_address
+    })
+    await model.set_config({
+        'juju-https-proxy': "https://%s:3128"
+        % proxy.public_address
+    })
+
+    kubernetes_worker = model.applications['kubernetes-worker']
+    kubernetes_master = model.applications['kubernetes-master']
+    # LP: https://bugs.launchpad.net/juju/+bug/1835050
+    await kubernetes_worker.set_config({'http_proxy': 'blah'})
+    await kubernetes_master.set_config({'http_proxy': 'blah'})
     time.sleep(20)
 
-########## Try this out. The charm should by default be OK to use.
-#     http_allow_all_conf = """
-# http_port 3128
-# acl all src 0.0.0.0/0
-# http_access allow all
-# """
-#     log('Catting config into proxy conf and restarting service.')
-#     log("proxy: %s " % "cat '%s' > /etc/squid/forwardproxy.conf && sudo service squid restart" % http_allow_all_conf)
-#     # Could be overwritten by charm.. need to update or create new charm for this.
-#     await proxy.ssh("sudo chmod -R 777 /etc/squid")
-#     with NamedTemporaryFile() as squid_conf:
-#         with open(squid_conf.name, 'w') as fp:
-#             fp.write(http_allow_all_conf)
-#         await proxy.scp_to(squid_conf.name, '/etc/squid/forwardproxy.conf')
-#     await proxy.ssh("sudo service squid restart")
+    log('waiting...')
+    await asyncify(_juju_wait)()
+    for worker_unit in model.applications['kubernetes-worker'].units:
+        await test_kube_node_conf(worker_unit)
+    for master_unit in model.applications['kubernetes-master'].units:
+        await test_kube_node_conf(master_unit)
 
+    await model.set_config({
+        'juju-http-proxy': ""
+    })
+    await model.set_config({
+        'juju-https-proxy': ""
+    })
 
+    # Config key must be different
+    await kubernetes_worker.set_config({'http_proxy': 'bla2h'})
+    await kubernetes_master.set_config({'http_proxy': 'bla2h'})
+    time.sleep(20)
     log('waiting...')
     await asyncify(_juju_wait)()
 
     for worker_unit in model.applications['kubernetes-worker'].units:
-        await test_kube_node_conf(worker_unit)
-    for worker_unit in model.applications['kubernetes-master'].units:
-        await test_kube_node_conf(worker_unit)
+        await test_kube_node_conf_missing(worker_unit)
+    for master_unit in model.applications['kubernetes-master'].units:
+        await test_kube_node_conf_missing(master_unit)
 
-    # # Cleanup
-    # await controller.destroy_model()
-    await proxy.remove()
-
-    log('waiting...')
-    await asyncify(_juju_wait)()
 
 @pytest.mark.asyncio
 async def test_juju_proxy_vars(log_dir):
@@ -112,5 +147,9 @@ async def test_juju_proxy_vars(log_dir):
     cloud = await controller.get_cloud()
     if cloud is not 'localhost':
         async with UseModel() as model:
-            await test_http_conf_existing_container_runtime(model)
-    await controller.destroy_model(model.info.uuid)
+            proxy_app = setup_proxy(model)
+            await test_http_conf_existing_container_runtime(
+                model,
+                proxy_app
+            )
+    await controller.destroy_model(model.get_info().uuid)
