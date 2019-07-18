5d66199ce7a452ca4ba15ef476ee92f41a87df58
Kenneth Koski <kenneth.koski@canonical.com>
2019-07-18 16:45:33 -0500

    Specify charm store namespace for Kubeflow bundle
    Otherwise it gets pushed up to the cdkbot namespace.

--- jobs/build-charms/kubeflow.groovy
+++ jobs/build-charms/kubeflow.groovy
@@ -67,7 +67,7 @@ pipeline {
         stage('Release Kubeflow Bundle') {
             steps {
                 exec 'git clone https://github.com/juju-solutions/bundle-kubeflow.git'
-                exec 'cd bundle-kubeflow && CHARM_BUILD_DIR=/tmp/charms juju bundle publish --url cs:kubeflow'
+                exec 'cd bundle-kubeflow && CHARM_BUILD_DIR=/tmp/charms juju bundle publish --url cs:~kubeflow-charmers/kubeflow'
             }
             when { expression { ゴゴゴ } }
         }
