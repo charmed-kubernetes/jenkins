bba1faff735744f0f8b927d0debd4086ee6bc51c
Peter J De Sousa <peter.de.sousa@canonical.com>
2019-07-24 09:10:22 +0100

    Update tests
    Change tests to check all profiles and find the correct one.

--- jobs/integration/test_lxd_profile_deploy.py
+++ jobs/integration/test_lxd_profile_deploy.py
@@ -25,84 +25,92 @@ LXD_PROFILE = yaml.load(
             "templates",
             "lxd-profile.yaml"
         ),
-        'r'
+        "r"
     )
 )
 
 
 async def check_charm_profile_deployed(app, charm_name):
     machine = app.units[0]
-    log('app_info %s' % machine.safe_data)
+    # log("app_info %s" % machine.safe_data)
     # Assume that the only profile with juju-* is
-    # the one we're looking for.
+    # the one we"re looking for.
     result = subprocess.run(
-        ['lxc', 'profile', 'list'],
+        ["lxc", "profile", "list"],
         stdout=subprocess.PIPE
     )
-    model_name = "NO-MODEL"
-    for profile_line in result.stdout.decode('utf-8').split('\n'):
-        match = re.search(
-            r"juju-(([a-zA-Z0-9])+-)*{}-[a-zA-Z0-9]+".format(
-                charm_name.split('-')[-1]
-            ),
-            profile_line
+
+    REGEX = r"(juju(-[a-zA-Z0-9]+)+(-[A-Za-z0-9]+)+(-[0-9])*)+"
+    matches = re.findall(
+        REGEX,
+        result.stdout.decode("utf-8")
+    )
+    log("[DEBUG] match: {}".format(str(matches)))
+
+
+    # Profiles can be in the form of
+    # juju-MODEL-NAME
+    # OR: juju-MODEL-NAME-APP
+    # Find the one which contains the profile
+    for profile_name in matches[0]:
+        log("Checking profile for name: %s" % profile_name)
+        # In 3.7 stdout=subprocess.PIPE
+        # can be replaced with capture_output... :-]
+        result = subprocess.run(
+            ["lxc", "profile", "show", profile_name],
+            stdout=subprocess.PIPE
         )
-        if match is not None:
-            model_name = match.group()
-            break
+        config = result.stdout.decode("utf-8")
 
-    log('Checking profile for name: %s' % model_name)
-    # In 3.7 stdout=subprocess.PIPE
-    # can be replaced with capture_output... :-]
-    result = subprocess.run(
-        ['lxc', 'profile', 'show', model_name],
-        stdout=subprocess.PIPE
-    )
+        loaded_yaml = yaml.load(config)
 
-    config = result.stdout.decode('utf-8')
+        if loaded_yaml is None:
+            continue
 
-    loaded_yaml = yaml.load(config)
+        def trim_profile(profile):
+            # Remove these keys as they differ at run time and are
+            # not related to the configuration.
+            profile.pop("name", None)
+            profile.pop("used_by", None)
+            return profile
 
-    # Remove these keys as they differ at run time and are
-    # not related to the configuration.
-    loaded_yaml.pop("name", None)
-    loaded_yaml.pop("used_by", None)
+        loaded_yaml = trim_profile(loaded_yaml)
+        lxd_profile = trim_profile(LXD_PROFILE)
 
-    log('Deployed Profile: %s' % loaded_yaml)
-    log('Expected Profile: %s' % LXD_PROFILE)
-    assert loaded_yaml == LXD_PROFILE
+        log("Deployed Profile: %s" % loaded_yaml)
+        log("Expected Profile: %s" % lxd_profile)
+        profile_exists = loaded_yaml == lxd_profile
 
+        if profile_exists:
+            assert profile_exists
+            log("profile exists")
+            return
 
-async def test_lxd_profile_deployed(model, charm_names, channel):
-    for name in charm_names:
-        app = model.applications[name]
-        await check_charm_profile_deployed(app, name)
+    assert False
 
 
-async def test_lxd_profile_deployed_upgrade(model, charm_names, channel):
-    for name in charm_names:
-        app = model.applications[name]
-        log('Upgrading charm to edge channel')
-        await app.upgrade_charm(channel=channel)
-        time.sleep(10)
-        log('Upgraded charm.')
-        await asyncify(_juju_wait)()
-        await check_charm_profile_deployed(app, name)
-
-
+@pytest.mark.parametrize(
+    "charm_name",
+    ["kubernetes-master", "kubernetes-worker"]
+)
 @pytest.mark.asyncio
-async def test_lxd_profiles(model):
-    await test_lxd_profile_deployed(
-        model,
-        ['kubernetes-worker', 'kubernetes-master'],
-        os.environ['CHARM_UPGRADE_CHANNEL']
-    )
+async def test_lxd_profiles(model, charm_name):
+    app = model.applications[charm_name]
+    await check_charm_profile_deployed(app, charm_name)
 
 
+@pytest.mark.parametrize(
+    "charm_name",
+    ("kubernetes-worker", "kubernetes-master")
+)
 @pytest.mark.asyncio
-async def test_lxd_profile_upgrade(model):
-    await test_lxd_profile_deployed_upgrade(
-        model,
-        ['kubernetes-worker', 'kubernetes-master'],
-        os.environ['CHARM_UPGRADE_CHANNEL']
-    )
+async def test_lxd_profile_upgrade(model, charm_name):
+    app = model.applications[charm_name]
+    log("Upgrading charm to edge channel")
+    # await app.upgrade_charm(channel="edge")
+    ## TESTING
+    app.upgrade_charm(path="/home/pjds/charms/builds/{}".format(charm_name))
+    time.sleep(10)
+    log("Upgraded charm.")
+    await asyncify(_juju_wait)()
+    await check_charm_profile_deployed(app, charm_name)
