d56d9952a91396b5e655a0ec74fe7665cb6c8676
Adam Stokes <battlemidget@users.noreply.github.com>
2019-08-09 17:37:06 -0400

    Make sure we're using latest charm info
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/build-charms/charms.py
+++ jobs/build-charms/charms.py
@@ -44,23 +44,19 @@ class CharmEnv:
             )
 
 
-def log(line):
-    click.echo(f"Charms :: {line}")
-
-
 def _push(repo_path, out_path, charm_entity, is_bundle=False):
     """ Pushes a built charm to Charmstore
     """
 
-    log(f"vcs: {repo_path} build-path: {out_path} {charm_entity}")
+    click.echo(f"vcs: {repo_path} build-path: {out_path} {charm_entity}")
     git_commit = sh.git("rev-parse", "HEAD", _cwd=repo_path)
     git_commit = git_commit.stdout.decode().strip()
-    log(f"grabbing git revision {git_commit}")
+    click.echo(f"grabbing git revision {git_commit}")
 
     resource_args = []
     if not is_bundle:
         # Build a list of `oci-image` resources that have `upstream-source` defined,
-        # which is added for this logic to work.
+        # which is added for this click.echoic to work.
         resources = yaml.safe_load(
             Path(out_path).joinpath("metadata.yaml").read_text()
         ).get("resources", {})
@@ -69,9 +65,9 @@ def _push(repo_path, out_path, charm_entity, is_bundle=False):
             for name, details in resources.items()
             if details["type"] == "oci-image" and details.get("upstream-source")
         }
-        log(f"Found {len(images)} oci-image resources:\n{pformat(images)}\n")
+        click.echo(f"Found {len(images)} oci-image resources:\n{pformat(images)}\n")
         for image in images.values():
-            log(f"Pulling {image}...")
+            click.echo(f"Pulling {image}...")
             sh.docker.pull(image)
 
         # Convert the image names and tags to `--resource foo=bar` format
@@ -83,11 +79,11 @@ def _push(repo_path, out_path, charm_entity, is_bundle=False):
         ]
 
     out = sh.charm.push(out_path, charm_entity, *resource_args)
-    log(f"Charm push returned: {out}")
+    click.echo(f"Charm push returned: {out}")
     # Output includes lots of ansi escape sequences from the docker push,
     # and we only care about the first line, which contains the url as yaml.
     out = yaml.safe_load(out.stdout.decode().strip().splitlines()[0])
-    log(f"Setting {out['url']} metadata: {git_commit}")
+    click.echo(f"Setting {out['url']} metadata: {git_commit}")
     sh.charm.set(out["url"], "commit={}".format(git_commit))
 
 
@@ -100,19 +96,19 @@ def _pull_layers(layer_index, layer_list, layer_branch, retries=15, timeout=60):
         if layer_name == "layer:index":
             continue
 
-        log(layer_name)
+        click.echo(layer_name)
 
         def download():
             for line in sh.charm(
-                "pull-source", "-v", "-i", layer_index, layer_name, _iter=True
+                    "pull-source", "-v", "-i", layer_index, layer_name, _iter=True, _bg_exc=False
             ):
                 click.echo(f" -- {line.strip()}")
 
         try:
             num_runs += 1
             download()
-        except sh.ErrorReturnCode_1 as e:
-            log(f"Problem: {e}, retrying [{num_runs}/{retries}]")
+        except sh.ErrorReturnCode as e:
+            click.echo(f"Problem: {e}, retrying [{num_runs}/{retries}]")
             if num_runs == retries:
                 raise SystemExit(f"Could not download charm after {retries} retries.")
             time.sleep(timeout)
@@ -187,7 +183,7 @@ def _resource(charm_entity, channel, builder, out_path, resource_spec):
     resources = yaml.safe_load(resources.stdout.decode())
     builder_sh = Path(builder).absolute()
     click.echo(builder_sh)
-    for line in sh.bash(str(builder_sh), _cwd=out_path, _iter=True, _err_to_out=True):
+    for line in sh.bash(str(builder_sh), _cwd=out_path, _iter=True, _bg_exc=False):
         click.echo(line.strip())
     for line in glob("{}/*".format(out_path)):
         resource_path = Path(line)
@@ -287,7 +283,7 @@ def build(
     _charm_list = yaml.safe_load(Path(charm_list).read_text(encoding="utf8"))
 
     _pull_layers(layer_index, layer_list, layer_branch)
-    log("charm builds")
+    click.echo("charm builds")
     for charm_map in _charm_list:
         for charm_name, charm_opts in charm_map.items():
             downstream = f"https://github.com/{charm_opts['downstream']}"
@@ -295,7 +291,7 @@ def build(
                 continue
 
             if dry_run:
-                log(
+                click.echo(
                     f"{charm_name:^25} :: vcs-branch: {charm_branch} to-channel: {to_channel} tags: {','.join(charm_opts['tags'])}"
                 )
                 continue
@@ -305,14 +301,14 @@ def build(
 
             dst_path = str(charm_env.build_dir / charm_name)
             for line in sh.git.clone(
-                "--branch", charm_branch, downstream, src_path, _iter=True
+                    "--branch", charm_branch, downstream, src_path, _iter=True, _bg_exc=False
             ):
-                log(line)
+                click.echo(line)
 
             for line in sh.charm.build(
-                r=True, force=True, _cwd=src_path, _iter=True, _err_to_out=True
+                r=True, force=True, _cwd=src_path, _iter=True, _bg_exc=True
             ):
-                log(line.strip())
+                click.echo(line.strip())
             sh.charm.proof(_cwd=dst_path)
             _push(src_path, dst_path, charm_entity)
             resource_builder = charm_opts.get("resource_build_sh", None)
@@ -338,7 +334,7 @@ def build(
     "--bundle-repo",
     required=True,
     help="upstream repo for bundle builder",
-    default="https://github.com/juju-solutions/bundle-canonical-kubernetes.git",
+    default="https://github.com/charmed-kubernetes/bundle-canonical-kubernetes.git",
 )
 @click.option(
     "--to-channel", required=True, help="channel to promote charm to", default="edge"
@@ -351,21 +347,21 @@ def build_bundles(bundle_list, filter_by_tag, bundle_repo, to_channel, dry_run):
 def _build_bundles(bundle_list, filter_by_tag, bundle_repo, to_channel, dry_run):
     charm_env = CharmEnv()
     _bundle_list = yaml.safe_load(Path(bundle_list).read_text(encoding="utf8"))
-    log("bundle builds")
+    click.echo("bundle builds")
     bundle_repo_dir = charm_env.tmp_dir / "bundles-kubernetes"
     bundle_build_dir = charm_env.tmp_dir / "tmp-bundles"
     sh.rm('-rf', bundle_repo_dir)
     sh.rm('-rf', bundle_build_dir)
     os.makedirs(str(bundle_repo_dir), exist_ok=True)
     os.makedirs(str(bundle_build_dir), exist_ok=True)
-    for line in sh.git.clone(bundle_repo, str(bundle_repo_dir), _iter=True):
-        log(line)
+    for line in sh.git.clone(bundle_repo, str(bundle_repo_dir), _iter=True, _bg_exc=False):
+        click.echo(line)
     for bundle_map in _bundle_list:
         for bundle_name, bundle_opts in bundle_map.items():
             if not any(match in filter_by_tag for match in bundle_opts["tags"]):
-                log(f"Skipping {bundle_name}")
+                click.echo(f"Skipping {bundle_name}")
                 continue
-            log(f"Processing {bundle_name}")
+            click.echo(f"Processing {bundle_name}")
             cmd = [
                 str(bundle_repo_dir / "bundle"),
                 "-o",
@@ -374,11 +370,11 @@ def _build_bundles(bundle_list, filter_by_tag, bundle_repo, to_channel, dry_run)
                 to_channel,
                 bundle_opts['fragments']
             ]
-            log(f"Running {' '.join(cmd)}")
+            click.echo(f"Running {' '.join(cmd)}")
             import subprocess
             subprocess.call(' '.join(cmd), shell=True)
             bundle_entity = f"cs:~{bundle_opts['namespace']}/{bundle_name}"
-            log(f"Check {bundle_entity}")
+            click.echo(f"Check {bundle_entity}")
             _push(
                 str(bundle_repo_dir), str(bundle_build_dir / bundle_name), bundle_entity, is_bundle=True
             )
