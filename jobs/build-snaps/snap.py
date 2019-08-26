d982e42af28d3e7cad6227dfa31c487252a66239
Adam Stokes <battlemidget@users.noreply.github.com>
2019-08-26 13:09:32 -0400

    set beta/edge on beta preleases
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/build-snaps/snap.py
+++ jobs/build-snaps/snap.py
@@ -67,8 +67,13 @@ def _sync_upstream(snap_list, starting_ver, force, patches, dry_run):
                 _fmt_version_str = f'{_fmt_version["major"]}.{_fmt_version["minor"]}'
                 tracks_to_publish = []
                 if _fmt_version["prerelease"]:
-                    click.echo(f"This is a pre-release")
-                    tracks_to_publish = [f"{_fmt_version_str}/edge"]
+                    if 'beta' in _fmt_version["prerelease"]:
+                        click.echo(f"This is a beta release, setting edge/beta tracks")
+                        tracks_to_publish = [f"{_fmt_version_str}/edge",
+                                             f"{_fmt_version_str}/beta"]
+                    if 'alpha' in _fmt_version["prerelease"]:
+                        click.echo(f"This is an alpha release, setting edge tracks")
+                        tracks_to_publish = [f"{_fmt_version_str}/edge"]
                 else:
                     tracks_to_publish = [
                         f"{_fmt_version_str}/edge",
