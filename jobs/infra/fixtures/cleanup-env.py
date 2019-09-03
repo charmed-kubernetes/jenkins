93115fc72a6ed4a3b79787c32235c5e166ffef9f
Adam Stokes <battlemidget@users.noreply.github.com>
2019-09-02 21:44:27 -0400

    cleanup controllers
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/fixtures/cleanup-env.py
+++ jobs/infra/fixtures/cleanup-env.py
@@ -1,31 +1,11 @@
-#!/usr/bin/env python3
-from subprocess import run, CalledProcessError
 import json
 import click
 
-def sh(args, **kwargs):
-    click.echo(args)
-    try:
-        output = run(args, capture_output=True, **kwargs).decode("utf-8")
-    except CalledProcessError as e:
-        msg = e.stdout.decode("utf-8") + e.stderr.decode("utf-8")
-        click.secho(msg, fg="red", bold=True)
-        raise
-    click.echo(output)
-    return output
-
-
-sh(["sudo", "apt", "clean"])
-sh(["rm", "-rf", "/var/log/*"])
-sh(["rm", "-rf", "/var/lib/jenkins/.cache/*"])
-sh(["sudo", "docker", "image", "prune", "-a", "--filter", "until=24h", "--force"])
-sh(["sudo", "docker", "container", "prune", "--filter", "until=24h", "--force"])
-sh(["du", "-h", "--max-depth=1", "/var/lib"])
-sh(["df", "-h"])
+import sh
 
 try:
     juju_controllers = json.loads(
-        sh(["juju", "controllers", "--format", "json"])
+        sh("juju", "controllers", "--format", "json")
     )
     if juju_controllers:
         for name, _ in juju_controllers:
@@ -37,30 +17,39 @@ try:
                     "-y",
                     name]
                 )
-            except CalledProcessError as e:
+            except sh.ErrorReturn as e:
                 click.echo(f"Error destroying {e}, continuing...")
-except CalledProcessError as e:
+except sh.ErrorReturnCodeas e:
     click.echo(f"Error reading controller: {e.stderr.decode()}, continuing...")
-containers = json.loads(sh("sudo", "lxc", "query", "/1.0/containers"))
+
+
+
+sh.sudo("apt", "clean")
+sh.sudo("rm", "-rf", "/var/log/*")
+sh("rm", "-rf", "/var/lib/jenkins/.cache/*")
+sh.sudo("docker", "image", "prune", "-a", "--filter", "until=24h", "--force")
+sh.sudo("docker", "container", "prune", "--filter", "until=24h", "--force")
+
+containers = json.loads(sh.sudo("lxc", "query", "/1.0/containers"))
 click.echo(containers)
 for container in containers:
-    sh(["sudo", "lxc", "--force", container])
+    sh.sudo("lxc", "--force", container)
 
-storage_pools = json.loads(sh(["sudo", "lxc", "query", "/1.0/storage-pools"]))
+storage_pools = json.loads(sh.sudo("sudo", "lxc", "query", "/1.0/storage-pools"))
 click.echo(storage_pools)
 for storage in storage_pools:
     storage_name = storage.split("/")[-1]
     volumes = json.loads(
-        sh(["sudo", "lxc", "query", f"/1.0/storage-pools/{storage_name}/volumes"])
+        sh.sudo("lxc", "query", f"/1.0/storage-pools/{storage_name}/volumes")
     )
     click.echo(volumes)
     for volume in volumes:
         volume_name = volume.split("/")[-1]
         click.echo(f"Deleting {volume_name}")
-        sh(["sudo", "lxc", "storage", "volume", "delete", storage_name, f"custom/{volume_name}"])
+        sh.sudo("lxc", "storage", "volume", "delete", storage_name, f"custom/{volume_name}")
     try:
-        sh(["sudo", "lxc", "storage", "delete", storage_name])
-    except CalledProcessError as e:
+        sh.sudo("lxc", "storage", "delete", storage_name)
+    except sh.ErrorReturnCode as e:
         click.echo(f"Error removing {e}, continuing...")
 
-sh(["rm", "-rf", "/var/lib/jenkins/venvs"])
+sh("rm", "-rf", "/var/lib/jenkins/venvs")
