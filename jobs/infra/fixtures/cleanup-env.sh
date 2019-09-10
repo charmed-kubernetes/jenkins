b71f7044dc471cb3a5b91b64e2e7bf492df092d2
Adam Stokes <battlemidget@users.noreply.github.com>
2019-09-10 08:55:04 -0400

    add cleanup job
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/fixtures/cleanup-env.sh
+++ jobs/infra/fixtures/cleanup-env.sh
@@ -14,5 +14,9 @@ sudo rm -rf /var/lib/jenkins/.cache/*
 docker image prune -a --filter until=24h --force
 docker container prune --filter until=24h --force
 rm -rf /var/lib/jenkins/venvs
+rm -rf /var/lib/jenkins/slaves/"$NODE_NAME"/workspace/* 2>&1
 
-for sid in `aws --region us-east-2 ec2 describe-subnets --query Subnets[].SubnetId --output text`; do aws --region us-east-2 ec2 delete-tags --resources $sid --tags Value=owned; done
+
+for sid in $(aws --region us-east-2 ec2 describe-subnets --query 'Subnets[].SubnetId' --output text); do
+    aws --region us-east-2 ec2 delete-tags --resources "$sid" --tags Value=owned
+done
