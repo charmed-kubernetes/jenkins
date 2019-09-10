85adaf3d0de2f8f8321bf25c13db16f630c30416
Adam Stokes <battlemidget@users.noreply.github.com>
2019-09-10 09:46:37 -0400

    try sudo
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/fixtures/cleanup-lxds.py
+++ jobs/infra/fixtures/cleanup-lxds.py
@@ -1,6 +1,6 @@
 import sh
 import json
-containers = sh.contrib.sudo.lxc.list('--format', 'json').stdout.decode()
+containers = sh.sudo.lxc.list('--format', 'json').stdout.decode()
 for container in containers:
-    for line in sh.contrib.sudo.lxc.delete('--force', container['name'], _iter=True, _bg_exc=False):
+    for line in sh.sudo.lxc.delete('--force', container['name'], _iter=True, _bg_exc=False):
         print(line)
