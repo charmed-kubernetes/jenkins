cda888f797780551158c229f70bae10b2372e1ad
Adam Stokes <battlemidget@users.noreply.github.com>
2019-09-10 09:47:51 -0400

    parse json
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/fixtures/cleanup-lxds.py
+++ jobs/infra/fixtures/cleanup-lxds.py
@@ -1,6 +1,7 @@
 import sh
 import json
 containers = sh.sudo.lxc.list('--format', 'json').stdout.decode()
+containers = json.loads(containers)
 for container in containers:
     for line in sh.sudo.lxc.delete('--force', container['name'], _iter=True, _bg_exc=False):
         print(line)
