d2890e3d0cceaf110774ba7f4428df0ca0e66ec1
Adam Stokes <battlemidget@users.noreply.github.com>
2019-09-19 16:28:17 +0200

    update promotion of charms/bundles
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/build-charms/promote-charms.groovy
+++ jobs/build-charms/promote-charms.groovy
@@ -1,6 +1,6 @@
 @Library('juju-pipeline@master') _
 
-def charm_sh = "${utils.cipy} build-charms/charms.py"
+def charm_sh = "python3 jobs/build-charms/charms.py"
 
 pipeline {
     agent { label 'runner-amd64' }
@@ -16,18 +16,11 @@ pipeline {
     }
     stages {
         stage('Release K8S charms to Store') {
-            when {
-                expression {
-                    return params.only_namespace == 'containers' || params.only_namespace == 'all'
-                }
-            }
             options {
                 timeout(time: 45, unit: 'MINUTES')
             }
             steps {
-                dir('jobs') {
-                    sh "${charm_sh} promote --charm-list includes/charm-support-matrix.inc --filter-by-tag ${params.tag} --from-channel ${params.from_channel} --to-channel ${params.to_channel}"
-                }
+                sh "${charm_sh} promote --charm-list jobs/includes/charm-support-matrix.inc --filter-by-tag ${params.tag} --from-channel ${params.from_channel} --to-channel ${params.to_channel} --filter-by-tag ${params.filter_by_tag}"
             }
         }
     }
