77a99895606264c0f9baea69da193de65359a8e7
Adam Stokes <battlemidget@users.noreply.github.com>
2019-10-23 12:07:56 -0400

    update eks builds
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/build-snaps/build-eks-snaps.py
+++ jobs/build-snaps/build-eks-snaps.py
@@ -13,6 +13,7 @@ import yaml
 import operator
 from lib import snapapi
 from pathlib import Path
+from sh.contrib import git
 
 
 def _set_snap_alias(build_path, alias):
@@ -37,29 +38,30 @@ def cli():
     "--build-path", required=True, default="release/snap", help="Path of snap builds"
 )
 @click.option(
-    "--version", required=True, default="1.12.9", help="Version of k8s to build"
+    "--version", required=True, default="1.14.8", help="Version of k8s to build"
 )
 @click.option(
     "--arch", required=True, default="amd64", help="Architecture to build against"
 )
 @click.option("--dry-run", is_flag=True)
-def build(snap, build_path, version, arch, match_re, rename_re, dry_run):
+def build(snap, build_path, version, arch, dry_run):
     """ Build snaps
 
     Usage:
 
-    snaps.py build --snap kubectl --snap kube-proxy --version 1.10.3 --arch amd64 --match-re '(?=\S*[-]*)([a-zA-Z-]+)(.*)' --rename-re '\1-eks'
-
-    Passing --rename-re and --match-re allows you to manipulate the resulting
-    snap file, for example, the above renames kube-proxy_1.10.3_amd64.snap to
-    kube-proxy-eks_1.10.3_amd64.snap
+    build-eks-snaps.py build \
+        --snap kubectl \
+        --snap kube-proxy \
+        --snap kubelet \
+        --snap kubernetes-test \
+        --version 1.14.8
     """
     if not version.startswith("v"):
         version = f"v{version}"
     env = os.environ.copy()
     env["KUBE_VERSION"] = version
     env["KUBE_ARCH"] = arch
-    sh.git.clone(
+    git.clone(
         "https://github.com/juju-solutions/release.git",
         build_path,
         branch="rye/snaps",
@@ -69,8 +71,7 @@ def build(snap, build_path, version, arch, match_re, rename_re, dry_run):
     snap_alias = None
 
     for _snap in snap:
-        if match_re and rename_re:
-            snap_alias = f"{_snap}-eks"
+        snap_alias = f"{_snap}-eks"
 
         if snap_alias:
             snapcraft_fn = build_path / f"{_snap}.yaml"
@@ -100,7 +101,7 @@ def build(snap, build_path, version, arch, match_re, rename_re, dry_run):
     default="release/snap/snap/build",
     help="Path of resulting snap builds",
 )
-@click.option("--version", required=True, default="1.12.9", help="k8s Version")
+@click.option("--version", required=True, default="1.14.8", help="k8s Version")
 @click.option("--dry-run", is_flag=True)
 def push(result_dir, version, dry_run):
     """ Promote to a snapstore channel/track
