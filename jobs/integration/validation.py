c433b4344a80df5a98757f84c0d95c183f73990c
Adam Stokes <battlemidget@users.noreply.github.com>
2020-02-19 20:15:54 -0500

    update specs for proper cleanup
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/integration/validation.py
+++ jobs/integration/validation.py
@@ -1489,7 +1489,6 @@ async def test_encryption_at_rest(model, tools):
 
 @pytest.mark.asyncio
 @pytest.mark.offline
-@pytest.mark.timeout(900)
 async def test_dns_provider(model, tools):
     master_app = model.applications["kubernetes-master"]
     master_unit = master_app.units[0]
