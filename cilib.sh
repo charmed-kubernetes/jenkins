ee62b0f83d1a19b32b6b55f5fc3b6bb5f6c49c63
Adam Stokes <battlemidget@users.noreply.github.com>
2020-02-24 14:08:43 -0500

    add bash helpers for execute
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ cilib.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -eu
+
+setup_env()
+{
+
+  export JUJU_CONTROLLER="$JUJU_CONTROLLER-$(ogc-collect get-key job_id | cut -f1 -d-)"
+
+  export JUJU_MODEL="$JUJU_MODEL-$SERIES-$(ogc-collect get-key job_id | cut -f1 -d-)"
+
+  ogc-collect set-key 'juju_controller' "$JUJU_CONTROLLER"
+  ogc-collect set-key 'juju_model' "$JUJU_MODEL"
+  ogc-collect set-key 'job_name_custom' "validate-ck-nfs-$SERIES-$SNAP_VERSION"
+  ogc-collect set-key 'snap_version' "$SNAP_VERSION"
+  ogc-collect set-key 'juju_deploy_channel' "$JUJU_DEPLOY_CHANNEL"
+  ogc-collect set-key 'series' "$SERIES"
+}
+
+
+inject_env()
+{
+    export JUJU_CONTROLLER=$(ogc-collect get-key juju_controller)
+    export JUJU_MODEL=$(ogc-collect get-key juju_model)
+}
+
+collect_env()
+{
+    juju-crashdump -a debug-layer -a config -m "$JUJU_CONTROLLER:$JUJU_MODEL"
+}
+
+teardown_env()
+{
+    juju destroy-controller -y --destroy-all-models --destroy-storage $JUJU_CONTROLLER
+}
+
+
+unitAddress()
+{
+    py_script="
+import sys
+import yaml
+
+status_yaml=yaml.safe_load(sys.stdin)
+unit = status_yaml['applications']['$1']['units']
+units = list(unit.keys())
+print(unit[units[0]]['public-address'])
+"
+    juju status -m "$JUJU_CONTROLLER:$JUJU_MODEL" "$1" --format yaml | env python3 -c "$py_script"
+}
