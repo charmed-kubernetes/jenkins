31453f6c5f299cb03f6a7a6720c0dfb5f203933f
Adam Stokes <battlemidget@users.noreply.github.com>
2020-02-25 16:32:40 -0500

    mark microbot flaky
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/integration/validation.py
+++ jobs/integration/validation.py
@@ -290,7 +290,7 @@ async def test_rbac_flag(model):
 
 @pytest.mark.asyncio
 @pytest.mark.skip_arch(["s390x", "arm64", "aarch64"])
-@pytest.mark.skip("Will retry with different dns resolver")
+@pytest.mark.flaky
 async def test_microbot(model, tools):
     """ Validate the microbot action """
     unit = model.applications["kubernetes-worker"].units[0]
