6598bcca6dacbdfc0a46c07315eb0f5f5b3e8e01
Adam Stokes <battlemidget@users.noreply.github.com>
2020-04-06 15:47:13 -0400

    build snap store build/upload report
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ cilib/html.py
@@ -0,0 +1,14 @@
+""" some helpers to write out html snippets
+"""
+import os
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+
+def template(name):
+    """ Returns a template from jobs/templates/<name>
+    """
+    files_p = str(Path(os.environ.get('WORKSPACE')) / 'jobs/templates')
+    env = Environment(loader=FileSystemLoader(files_p))
+    _tmpl = env.get_template(name)
+    return _tmpl
--- jobs/build-snaps/snap.py
+++ jobs/build-snaps/snap.py
@@ -9,15 +9,17 @@ import os
 import yaml
 import semver
 import tempfile
+import itertools
+from pprint import pprint, pformat
 from sh.contrib import git
 from urllib.parse import urlparse
 from jinja2 import Template
 from pathlib import Path
 from pymacaroons import Macaroon
-from cilib import lp, idm, snapapi
+from cilib import lp, idm, snapapi, html
 from cilib.git import remote_branches, branch_exists, remote_tags
 from cilib.run import cmd_ok, capture
-
+from datetime import datetime
 
 # go compiler version map for k8s version releases
 K8S_GO_MAP = {
@@ -462,6 +464,58 @@ def release(name, channel, version, dry_run):
         click.echo(
             sh.snapcraft.release(name, latest_release["rev"], channel, _err_to_out=True)
         )
+
+
+@cli.command()
+@click.option("--snap-list", help="Path to supported snaps", required=True)
+@click.option("--snap-versions", help="Path to supported snap versions", required=True)
+@click.option("--owner", help="Owner who can access builds", required=True, default="k8s-jenkaas-admins")
+def build_summaries(snap_list, snap_versions, owner):
+    """ Return snap build summaries
+    """
+    _client = lp.Client(stage="production")
+    _client.login()
+
+    snap_list_p = Path(snap_list)
+    snap_versions_p = Path(snap_versions)
+
+    snap_iter = yaml.safe_load(snap_list_p.read_text())
+    snap_versions_iter = yaml.safe_load(snap_versions_p.read_text())
+
+    owner_link = _client.owner(owner)
+
+    snaps_to_process = [
+        f"{name}-{ver}"
+        for name, ver in list(itertools.product(*[snap_iter, snap_versions_iter]))
+    ]
+
+
+    summaries = []
+    for item in snaps_to_process:
+        builds = _client.snaps.getByName(name=item, owner=owner_link).builds[:4]
+
+        for build in builds:
+            arch = build.distro_arch_series.architecture_tag
+            click.echo(f"Summarizing {item} - {arch}")
+            summaries.append({
+                "name": f"{item}-{arch}",
+                "created": build.datecreated.strftime("%Y-%m-%d %H:%M:%S"),
+                "started": build.date_started.strftime("%Y-%m-%d %H:%M:%S"),
+                "finished": build.datebuilt.strftime("%Y-%m-%d %H:%M:%S"),
+                "buildstate": build.buildstate,
+                "build_log_url": build.build_log_url,
+                "store_upload_status": build.store_upload_status,
+                "store_upload_errors": build.store_upload_error_messages,
+                "upload_log_url": build.upload_log_url,
+                "channels": build.snap.store_channels
+            })
+
+    tmpl = html.template("snap_summary.html")
+    rendered = tmpl.render({'rows': summaries})
+
+    summary_html_p = Path("snap_summary.html")
+    summary_html_p.write_text(rendered)
+    cmd_ok("aws s3 cp snap_summary.html s3://jenkaas/snap_summary.html", shell=True)
 
 
 if __name__ == "__main__":
