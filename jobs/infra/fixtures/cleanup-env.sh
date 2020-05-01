515512a4ab0ee45d72006de712eb1c4bf89a9fc5
Adam Stokes <battlemidget@users.noreply.github.com>
2020-05-01 11:49:39 -0400

    more parallel
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/fixtures/cleanup-env.sh
+++ jobs/infra/fixtures/cleanup-env.sh
@@ -19,29 +19,11 @@ docker container prune --filter until=24h --force
 rm -rf /var/lib/jenkins/venvs
 
 aws --region us-east-1 ec2 describe-instances | jq '.Reservations[].Instances[] | select(contains({Tags: [{Key: "owner"} ]}) | not)' | jq -r '.InstanceId' | parallel -n10 aws --region us-east-1 ec2 terminate-instances --instance-ids {}
-
-for iid in $(aws --region us-east-2 ec2 describe-instances | jq '.Reservations[].Instances[] | select(contains({Tags: [{Key: "owner"} ]}) | not)' | jq -r '.InstanceId'); do
-    aws --region us-east-2 ec2 terminate-instances --instance-ids "$iid"
-done
-
-for sid in $(aws --region us-east-2 ec2 describe-subnets --query 'Subnets[].SubnetId' --output text); do
-    aws --region us-east-2 ec2 delete-tags --resources "$sid" --tags Value=owned
-done
-
-for sg in $(aws --region us-east-1 ec2 describe-security-groups --filters Name=owner-id,Values=018302341396 --query "SecurityGroups[*].{Name:GroupId}" --output text); do
-    aws --region us-east-1 ec2 delete-security-group --group-id "$sg"
-done
-
-for sg in $(aws --region us-east-2 ec2 describe-security-groups --filters Name=owner-id,Values=018302341396 --query "SecurityGroups[*].{Name:GroupId}" --output text); do
-    aws --region us-east-2 ec2 delete-security-group --group-id "$sg"
-done
-
-
-for cntr in $(sudo lxc list --format json | jq -r ".[] | .name"); do
-    echo "Removing $cntr"
-    sudo lxc delete --force "$cntr"
-done
-
+aws --region us-east-2 ec2 describe-instances | jq '.Reservations[].Instances[] | select(contains({Tags: [{Key: "owner"} ]}) | not)' | jq -r '.InstanceId' | parallel -n10 aws --region us-east-2 ec2 terminate-instances --instance-ids {}
+aws --region us-east-2 ec2 describe-subnets --query 'Subnets[].SubnetId' --output text | parallel -n10 aws --region us-east-2 ec2 delete-tags --resources {} --tags Value=owned
+aws --region us-east-1 ec2 describe-security-groups --filters Name=owner-id,Values=018302341396 --query "SecurityGroups[*].{Name:GroupId}" --output text | parallel -n10 aws --region us-east-1 ec2 delete-security-group --group-id {}
+aws --region us-east-2 ec2 describe-security-groups --filters Name=owner-id,Values=018302341396 --query "SecurityGroups[*].{Name:GroupId}" --output text | parallel -n10 aws --region us-east-2 ec2 delete-security-group --group-id {}
+sudo lxc list --format json | jq -r ".[] | .name" | parallel -n10 sudo lxc delete --force {}
 for cntr in $(sudo lxc profile list --format json | jq -r ".[] | .name"); do
     if [[ $cntr != "default" ]]; then
 	    echo "Removing $cntr"
