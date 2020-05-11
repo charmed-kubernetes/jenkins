e6399d0a7463e43f049b2fb872beeeec88227dc6
Adam Stokes <battlemidget@users.noreply.github.com>
2020-05-11 12:03:32 -0400

    update localhost spec
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- juju.bash
+++ juju.bash
@@ -59,3 +59,17 @@ function juju::wait
     echo "Waiting for deployment to settle..."
     timeout 45m juju-wait -e "$JUJU_CONTROLLER:$JUJU_MODEL" -w
 }
+
+function juju::unitAddress
+{
+    py_script="
+import sys
+import yaml
+
+status_yaml=yaml.safe_load(sys.stdin)
+unit = status_yaml['applications']['$1']['units']
+units = list(unit.keys())
+print(unit[units[0]]['public-address'])
+"
+    juju status -m "$JUJU_CONTROLLER:$JUJU_MODEL" "$1" --format yaml | env python3 -c "$py_script"
+}
