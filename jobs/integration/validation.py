b2fa543cc4c0b348b26a7ba6e0d873e253ee1b3b
Adam Stokes <battlemidget@users.noreply.github.com>
2020-05-12 14:03:59 -0400

    bring in test_containerd
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/integration/validation.py
+++ jobs/integration/validation.py
@@ -1931,3 +1931,46 @@ async def test_cinder(model, tools):
     await validate_storage_class(model, "cdk-cinder", "Cinder")
     # cleanup
     await model.applications["openstack-integrator"].destroy()
+
+
+@pytest.mark.asyncio
+async def test_containerd_to_docker(model, tools):
+    """
+    Assume we're starting with containerd, replace
+    with Docker and then revert to containerd.
+
+    :param model: Object
+    :return: None
+    """
+    containerd_app = model.applications["containerd"]
+
+    await containerd_app.remove()
+    await tools.juju_wait("-x", "kubernetes-worker")
+    # Block until containerd's removed, ignore `blocked` worker.
+
+    docker_app = await model.deploy(
+        "cs:~containers/docker", num_units=0, channel="edge"  # Subordinate.
+    )
+
+    await docker_app.add_relation("docker", "kubernetes-master")
+
+    await docker_app.add_relation("docker", "kubernetes-worker")
+
+    await tools.juju_wait()
+    # If we settle, it's safe to
+    # assume Docker is now running
+    # workloads.
+
+    await docker_app.remove()
+    await tools.juju_wait("-x", "kubernetes-worker")
+    # Block until docker's removed, ignore `blocked` worker.
+
+    containerd_app = await model.deploy(
+        "cs:~containers/containerd", num_units=0, channel="edge"  # Subordinate.
+    )
+
+    await containerd_app.add_relation("containerd", "kubernetes-master")
+
+    await containerd_app.add_relation("containerd", "kubernetes-worker")
+
+    await tools.juju_wait()
