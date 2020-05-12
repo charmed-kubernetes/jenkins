5530b0466483cd675129022834e663958714d013
Adam Stokes <battlemidget@users.noreply.github.com>
2020-05-12 08:59:49 -0400

    fix ev closing
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/integration/validation.py
+++ jobs/integration/validation.py
@@ -1876,58 +1876,59 @@ async def test_multus(model, tools, addons_model):
     await cleanup()
 
 
-@pytest.mark.asyncio
-async def test_series_upgrade(model, tools):
-    if not tools.is_series_upgrade:
-        pytest.skip("No series upgrade argument found")
-    k8s_master_0 = model.applications['kubernetes-master'].units[0]
-    old_series = k8s_master_0.machine.series
-    try:
-        new_series = SERIES_ORDER[SERIES_ORDER.index(old_series) + 1]
-    except IndexError:
-        pytest.skip("no supported series to upgrade to")
-    except ValueError:
-        pytest.skip("unrecognized series to upgrade from: {old_series}")
-    for machine in model.machines.values():
-        await prep_series_upgrade(machine, new_series, tools)
-        await do_series_upgrade(machine)
-        await finish_series_upgrade(machine, tools)
-        assert machine.series == new_series
-    expected_messages = {
-        "kubernetes-master": "Kubernetes master running.",
-        "kubernetes-worker": "Kubernetes worker running.",
-    }
-    for app, message in expected_messages.items():
-        for unit in model.applications[app].units:
-            assert unit.workload_status_message == message
-
-
+# This is definitely causing the EV to close prematurely
 # @pytest.mark.asyncio
-# @pytest.mark.clouds(['openstack'])
-# async def test_cinder(model, tools):
-#     # setup
-#     log.info("deploying openstack-integrator")
-#     series = 'bionic'
-#     await model.deploy(
-#         "openstack-integrator",
-#         num_units=1,
-#         series=series,
-#         trust=True,
-#     )
+# async def test_series_upgrade(model, tools):
+#     if not tools.is_series_upgrade:
+#         pytest.skip("No series upgrade argument found")
+#     k8s_master_0 = model.applications['kubernetes-master'].units[0]
+#     old_series = k8s_master_0.machine.series
+#     try:
+#         new_series = SERIES_ORDER[SERIES_ORDER.index(old_series) + 1]
+#     except IndexError:
+#         pytest.skip("no supported series to upgrade to")
+#     except ValueError:
+#         pytest.skip("unrecognized series to upgrade from: {old_series}")
+#     for machine in model.machines.values():
+#         await prep_series_upgrade(machine, new_series, tools)
+#         await do_series_upgrade(machine)
+#         await finish_series_upgrade(machine, tools)
+#         assert machine.series == new_series
+#     expected_messages = {
+#         "kubernetes-master": "Kubernetes master running.",
+#         "kubernetes-worker": "Kubernetes worker running.",
+#     }
+#     for app, message in expected_messages.items():
+#         for unit in model.applications[app].units:
+#             assert unit.workload_status_message == message
 
-#     log.info("adding relations")
-#     await model.add_relation("openstack-integrator", "kubernetes-master")
-#     await model.add_relation("openstack-integrator", "kubernetes-worker")
-#     log.info("waiting...")
-#     await tools.juju_wait()
 
-#     log.info("waiting for csi to settle")
-#     unit = model.applications["kubernetes-master"].units[0]
-#     await retry_async_with_timeout(
-#         verify_ready, (unit, "po", ["csi-cinder-controllerplugin-0"], "-n kube-system"),
-#         timeout_msg="CSI pod not ready!"
-#     )
-#     # create pod that writes to a pv from cinder
-#     await validate_storage_class(model, "cdk-cinder", "Cinder")
-#     # cleanup
-#     await model.applications["openstack-integrator"].destroy()
+@pytest.mark.asyncio
+@pytest.mark.clouds(['openstack'])
+async def test_cinder(model, tools):
+    # setup
+    log.info("deploying openstack-integrator")
+    series = 'bionic'
+    await model.deploy(
+        "openstack-integrator",
+        num_units=1,
+        series=series,
+        trust=True,
+    )
+
+    log.info("adding relations")
+    await model.add_relation("openstack-integrator", "kubernetes-master")
+    await model.add_relation("openstack-integrator", "kubernetes-worker")
+    log.info("waiting...")
+    await tools.juju_wait()
+
+    log.info("waiting for csi to settle")
+    unit = model.applications["kubernetes-master"].units[0]
+    await retry_async_with_timeout(
+        verify_ready, (unit, "po", ["csi-cinder-controllerplugin-0"], "-n kube-system"),
+        timeout_msg="CSI pod not ready!"
+    )
+    # create pod that writes to a pv from cinder
+    await validate_storage_class(model, "cdk-cinder", "Cinder")
+    # cleanup
+    await model.applications["openstack-integrator"].destroy()
