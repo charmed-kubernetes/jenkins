770c7a916c059164d7f4eb688791628807c453aa
Adam Stokes <battlemidget@users.noreply.github.com>
2020-05-13 10:38:26 -0400

    dont retry hooks
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- juju.bash
+++ juju.bash
@@ -22,7 +22,8 @@ function juju::bootstrap
          --bootstrap-constraints arch="$ARCH" \
          --model-default test-mode=true \
          --model-default resource-tags=owner=k8sci \
-         --model-default image-stream=daily
+         --model-default image-stream=daily \
+         --model-default automatically-retry-hooks=false
 }
 
 function juju::deploy::before
