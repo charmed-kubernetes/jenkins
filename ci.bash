58bbb1f160e331bd6ceb866c6b01b7ab0a860460
Adam Stokes <battlemidget@users.noreply.github.com>
2020-05-13 13:03:52 -0400

    update deps
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- ci.bash
+++ ci.bash
@@ -24,22 +24,21 @@ function compile::env
     export JUJU_MODEL
     export SERIES
     export SNAP_VERSION
-    export TMPDIR
 
     echo "Storing initial meta information"
     local job_name_format
     local snap_version_format
 
-    mkdir -p "$TMPDIR/meta"
+    mkdir -p "meta"
     job_name_format=$(echo "$JOB_NAME_CUSTOM" | tr '/' '-')
     snap_version_format=$(echo "$SNAP_VERSION" | tr '/' '-')
 
-    touch "$TMPDIR/meta/name-$job_name_format"
-    touch "$TMPDIR/meta/channel-$JUJU_DEPLOY_CHANNEL"
-    touch "$TMPDIR/meta/series-$SERIES"
-    touch "$TMPDIR/meta/snap_version-$snap_version_format"
-    for i in "$TMPDIR"/meta/*; do
-        "$venv_p/bin/aws" s3 cp "$i" "s3://jenkaas/$JOB_ID/meta/"
+    touch "meta/name-$job_name_format"
+    touch "meta/channel-$JUJU_DEPLOY_CHANNEL"
+    touch "meta/series-$SERIES"
+    touch "meta/snap_version-$snap_version_format"
+    for i in meta/*; do
+        aws s3 cp "$i" "s3://jenkaas/$JOB_ID/meta/"
     done
 }
 
@@ -66,7 +65,7 @@ function test::execute
 {
     declare -n is_pass=$1
     timeout 2h pytest \
-        --html="$TMPDIR/report.html" \
+        --html="report.html" \
         jobs/integration/validation.py \
         --cloud "$JUJU_CLOUD" \
         --model "$JUJU_MODEL" \
@@ -85,34 +84,26 @@ function test::report
     build_starttime=$2
     deploy_endtime=$3
 
-    python -c "import json; from datetime import datetime; print(json.dumps({'test_result': $result, 'job_name_custom': '$JOB_NAME_CUSTOM', 'job_name': '$JOB_NAME_CUSTOM', 'job_id': '$JOB_ID', 'build_endtime': datetime.utcnow().isoformat(), 'build_starttime': '$build_starttime', 'deploy_endtime': '$deploy_endtime'}))" | tee "$TMPDIR/metadata.json"
+    python -c "import json; from datetime import datetime; print(json.dumps({'test_result': $result, 'job_name_custom': '$JOB_NAME_CUSTOM', 'job_name': '$JOB_NAME_CUSTOM', 'job_id': '$JOB_ID', 'build_endtime': datetime.utcnow().isoformat(), 'build_starttime': '$build_starttime', 'deploy_endtime': '$deploy_endtime'}))" | tee "metadata.json"
 }
 
 function test::capture
 {
     if which juju-crashdump; then
-        juju-crashdump -s -a debug-layer -a config -m "$JUJU_CONTROLLER:$JUJU_MODEL" -o "$TMPDIR"
+        juju-crashdump -s -a debug-layer -a config -m "$JUJU_CONTROLLER:$JUJU_MODEL"
     fi
-    (cd $TMPDIR && tar --exclude='venv' -cvzf artifacts.tar.gz *)
-    "$venv_p/bin/columbo" --output-dir "$TMPDIR/_out" "$TMPDIR/artifacts.tar.gz" || true
-    "$venv_p/bin/aws" s3 cp "$TMPDIR/_out/columbo-report.json" s3://jenkaas/"$JOB_ID"/columbo-report.json || true
-    "$venv_p/bin/aws" s3 cp "$TMPDIR/metadata.json" s3://jenkaas/"$JOB_ID"/metadata.json || true
-    "$venv_p/bin/aws" s3 cp "$TMPDIR/report.html" s3://jenkaas/"$JOB_ID"/index.html || true
-    "$venv_p/bin/aws" s3 cp "$TMPDIR/artifacts.tar.gz" s3://jenkaas/"$JOB_ID"/artifacts.tar.gz || true
+    tar --exclude='venv' -cvzf artifacts.tar.gz *
+    columbo --output-dir "_out" "artifacts.tar.gz" || true
+    aws s3 cp "_out/columbo-report.json" s3://jenkaas/"$JOB_ID"/columbo-report.json || true
+    aws s3 cp "metadata.json" s3://jenkaas/"$JOB_ID"/metadata.json || true
+    aws s3 cp "report.html" s3://jenkaas/"$JOB_ID"/index.html || true
+    aws s3 cp "artifacts.tar.gz" s3://jenkaas/"$JOB_ID"/artifacts.tar.gz || true
 }
 
 
 # Entrypoint to start the deployment, testing, reporting
 function ci::run
 {
-    ci::venv venv_p
-    set +u
-    source "$venv_p"/bin/activate
-    set -u
-
-    pip install tox pip-tools
-    pip-sync requirements.txt
-
     compile::env
 
     local log_name_custom=$(echo "$JOB_NAME_CUSTOM" | tr '/' '-')
@@ -134,7 +125,7 @@ function ci::run
         test::capture
         ci::cleanup::before
 
-    } 2>&1 | sed -u -e "s/^/[$log_name_custom] /" | tee -a "$TMPDIR/ci.log"
+    } 2>&1 | sed -u -e "s/^/[$log_name_custom] /" | tee -a "ci.log"
 }
 
 # get latest python on system
@@ -155,6 +146,8 @@ function ci::venv
 {
     declare -n _venv_p=$1
     _venv_p="$TMPDIR/venv"
+
+    rm -rf "$TMPDIR/venv" || true
 
     virtualenv "$_venv_p" -p "$(ci::py)"
 }
@@ -183,7 +176,7 @@ function ci::cleanup
         if ! timeout 2m juju destroy-controller -y --destroy-all-models --destroy-storage "$JUJU_CONTROLLER"; then
             timeout 2m juju kill-controller -y "$JUJU_CONTROLLER" || true
         fi
-    } 2>&1 | sed -u -e "s/^/[$log_name_custom] /" | tee -a "$TMPDIR/ci.log"
+    } 2>&1 | sed -u -e "s/^/[$log_name_custom] /" | tee -a "ci.log"
 }
 trap ci::cleanup EXIT
 
