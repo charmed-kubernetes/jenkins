a1911efc9a7ca2b6252d234c3a65ba1bfe669538
Adam Stokes <battlemidget@users.noreply.github.com>
2020-06-10 15:46:29 -0400

    i learnt bash
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/fixtures/cleanup-env.sh
+++ jobs/infra/fixtures/cleanup-env.sh
@@ -32,7 +32,7 @@ rm -rf /var/lib/jenkins/.tox
 
 regions=(us-east-1 us-east-2 us-west-1)
 
-for region in regions; do
+for region in ${regions[@]}; do
     aws --region "$region" ec2 describe-instances | jq '.Reservations[].Instances[] | select(contains({Tags: [{Key: "owner"} ]}) | not)' | jq -r '.InstanceId' | parallel aws --region "$region" ec2 terminate-instances --instance-ids {}
     aws --region "$region" ec2 describe-subnets --query 'Subnets[].SubnetId' --output json | jq -r '.[]' | parallel aws --region "$region" ec2 delete-tags --resources {} --tags Value=owned
     aws --region "$region" ec2 describe-security-groups --filters Name=owner-id,Values=018302341396 --query "SecurityGroups[*].{Name:GroupId}" --output json | jq -r '.[].Name' | parallel aws --region "$region" ec2 delete-security-group --group-id "{}"
