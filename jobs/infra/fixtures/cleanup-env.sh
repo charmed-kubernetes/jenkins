fbd3f1584ef84cd401514c5105bec9c80677c51d
Adam Stokes <battlemidget@users.noreply.github.com>
2020-06-11 15:42:03 -0400

    update timeouts in cleanup
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/infra/fixtures/cleanup-env.sh
+++ jobs/infra/fixtures/cleanup-env.sh
@@ -5,8 +5,8 @@ function purge::controllers
 {
     if [ "$1" != "jaas" ]; then
         echo "$1"
-        if ! timeout 15m juju destroy-controller -y --destroy-all-models --destroy-storage "$1"; then
-            timeout 15m juju kill-controller -y "$1" 2>&1
+        if ! timeout 2m juju destroy-controller -y --destroy-all-models --destroy-storage "$1"; then
+            timeout 5m juju kill-controller -y "$1" 2>&1
         fi
     fi
 }
