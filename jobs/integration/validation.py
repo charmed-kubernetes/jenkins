fafe1cdbf6efec2cf23763499c33513d28ad40cb
Adam Stokes <battlemidget@users.noreply.github.com>
2020-06-17 12:29:40 -0400

    update tests, combine additional permutations into one
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- jobs/integration/validation.py
+++ jobs/integration/validation.py
@@ -28,12 +28,15 @@ from .utils import (
     verify_deleted,
     verify_ready,
     is_localhost,
-    #    validate_storage_class,
+    validate_storage_class,
     #    SERIES_ORDER,
     #    prep_series_upgrade,
     #    do_series_upgrade,
     #    finish_series_upgrade,
 )
+import urllib.request
+from .logger import log
+from bs4 import BeautifulSoup as bs
 
 # Quiet the noise
 logging.getLogger("websockets.protocol").setLevel(logging.INFO)
@@ -1868,6 +1871,242 @@ async def test_multus(model, tools, addons_model):
             )
 
     await cleanup()
+
+
+def find_nagios_criticals(url, opener):
+    url_data = opener.open(url)
+    soup = bs(url_data.read(), "html.parser")
+    return soup.find_all("td", class_="statusBGCRITICAL")
+
+
+async def wait_for_no_errors(url, opener):
+    criticals = ["dummy"]
+    while len(criticals) > 0:
+        criticals = find_nagios_criticals(url, opener)
+        await asyncio.sleep(30)
+
+
+@pytest.mark.asyncio
+async def test_nagios(model, tools):
+    """ This test verifies the nagios relation is working
+    properly. This requires:
+
+    1) Deploy nagios and nrpe
+    2) login to nagios
+    3) verify things settle and no errors
+    4) force api server issues
+    5) verify nagios errors show for master and worker
+    6) fix api server
+    7) break a worker's kubelet
+    8) verify nagios errors for worker
+    9) fix worker
+    """
+
+    log("starting nagios test")
+    masters = model.applications["kubernetes-master"]
+    k8s_version_str = masters.data["workload-version"]
+    k8s_minor_version = tuple(int(i) for i in k8s_version_str.split(".")[:2])
+    if k8s_minor_version < (1, 17):
+        log("skipping, k8s version v" + k8s_version_str)
+        return
+
+    # 1) deploy
+    log("deploying nagios and nrpe")
+    nagios = await model.deploy("nagios", series="bionic")
+    await model.deploy(
+        "nrpe", series="bionic", config={"swap": "", "swap_activity": ""}, num_units=0
+    )
+    await nagios.expose()
+    await model.add_relation("nrpe", "kubernetes-master")
+    await model.add_relation("nrpe", "kubernetes-worker")
+    await model.add_relation("nrpe", "etcd")
+    await model.add_relation("nrpe", "easyrsa")
+    await model.add_relation("nrpe", "kubeapi-load-balancer")
+    await model.add_relation("nagios", "nrpe")
+    log("waiting for cluster to settle...")
+    await tools.juju_wait()
+
+    # 2) login to nagios
+    cmd = "cat /var/lib/juju/nagios.passwd"
+    output = await nagios.units[0].run(cmd, timeout=10)
+    assert output.status == "completed"
+    login_passwd = output.results.get("Stdout", "").strip()
+
+    pwd_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    url_base = "http://{}".format(nagios.units[0].public_address)
+    pwd_mgr.add_password(None, url_base, "nagiosadmin", login_passwd)
+    handler = urllib.request.HTTPBasicAuthHandler(pwd_mgr)
+    opener = urllib.request.build_opener(handler)
+    status_url = "{}/cgi-bin/nagios3/status.cgi?host=all".format(url_base)
+
+    # 3) wait for nagios to settle
+    log("waiting for nagios to settle")
+    await wait_for_no_errors(status_url, opener)
+
+    # 4) break all the things
+    log("breaking api server")
+    await masters.set_config({"api-extra-args": "broken=true"})
+
+    # 5) make sure nagios is complaining for kubernetes-master
+    #    AND kubernetes-worker
+    log("Verifying complaints")
+    criticals = []
+    while True:
+        criticals = find_nagios_criticals(status_url, opener)
+
+        if criticals:
+            found_master = []
+            found_worker = []
+            for c in criticals:
+                for link in c.find_all("a", recursive=False):
+                    if "kubernetes-master" in link.string:
+                        found_master.append(link.string)
+                    elif "kubernetes-worker" in link.string:
+                        found_worker.append(link.string)
+            if found_master and found_worker:
+                log("Found critical errors:")
+                for s in found_master + found_worker:
+                    log(" - {}".format(s))
+                break
+        await asyncio.sleep(30)
+
+    # 6) fix api and wait for settle
+    log("Fixing API server")
+    await masters.set_config({"api-extra-args": ""})
+    await wait_for_no_errors(status_url, opener)
+
+    # 7) break worker
+    log("Breaking workers")
+    workers = masters = model.applications["kubernetes-worker"]
+    await workers.set_config({"kubelet-extra-args": "broken=true"})
+
+    # 8) verify nagios is complaining about worker
+    log("Verifying complaints")
+    criticals = []
+    while True:
+        criticals = find_nagios_criticals(status_url, opener)
+
+        if criticals:
+            found_worker = []
+            for c in criticals:
+                for link in c.find_all("a", recursive=False):
+                    if "kubernetes-worker" in link.string:
+                        found_worker.append(link.string)
+                        break
+            if found_worker:
+                log("Found critical errors:")
+                for s in found_worker:
+                    log(" - {}".format(s))
+                break
+        await asyncio.sleep(30)
+
+    # 9) Fix worker and wait for complaints to go away
+    await workers.set_config({"kubelet-extra-args": ""})
+    await wait_for_no_errors(status_url, opener)
+
+
+@pytest.mark.asyncio
+async def test_nfs(model, tools):
+    # setup
+    log("deploying nfs")
+    await model.deploy("nfs")
+
+    log("adding relations")
+    await model.add_relation("nfs", "kubernetes-worker")
+    log("waiting...")
+    await tools.juju_wait()
+
+    log("waiting for nfs pod to settle")
+    unit = model.applications["kubernetes-master"].units[0]
+    await retry_async_with_timeout(
+        verify_ready,
+        (unit, "po", ["nfs-client-provisioner"]),
+        timeout_msg="NFS pod not ready!",
+    )
+    # create pod that writes to a pv from nfs
+    # yep, I called it default :-/
+    await validate_storage_class(model, "default", "NFS")
+
+    # cleanup
+    await model.applications["nfs"].destroy()
+    await tools.juju_wait()
+
+
+@pytest.mark.asyncio
+async def test_ceph(model, tools):
+    # setup
+    log.info("adding cloud:train to k8s-master")
+    series = os.environ["SERIES"]
+    check_cephfs = os.environ["SNAP_VERSION"].split("/")[0] not in ("1.15", "1.16")
+    await model.applications["kubernetes-master"].set_config(
+        {"install_sources": "[cloud:{}-train]".format(series)}
+    )
+    await tools.juju_wait()
+    log.info("deploying ceph mon")
+    await model.deploy(
+        "ceph-mon",
+        num_units=3,
+        series=series,
+        config={"source": "cloud:{}-train".format(series)},
+    )
+    cs = {
+        "osd-devices": {"size": 8 * 1024, "count": 1},
+        "osd-journals": {"size": 8 * 1024, "count": 1},
+    }
+    log.info("deploying ceph osd")
+    await model.deploy(
+        "ceph-osd",
+        storage=cs,
+        num_units=3,
+        series=series,
+        config={"source": "cloud:{}-train".format(series)},
+    )
+    if check_cephfs:
+        log.info("deploying ceph fs")
+        await model.deploy(
+            "ceph-fs",
+            num_units=1,
+            series=series,
+            config={"source": "cloud:{}-train".format(series)},
+        )
+
+    log.info("adding relations")
+    await model.add_relation("ceph-mon", "ceph-osd")
+    if check_cephfs:
+        await model.add_relation("ceph-mon", "ceph-fs")
+    await model.add_relation("ceph-mon:admin", "kubernetes-master")
+    await model.add_relation("ceph-mon:client", "kubernetes-master")
+    log.info("waiting...")
+    await tools.juju_wait()
+
+    # until bug https://bugs.launchpad.net/charm-kubernetes-master/+bug/1824035 fixed
+    unit = model.applications["ceph-mon"].units[0]
+    action = await unit.run_action("create-pool", name="ext4-pool")
+    await action.wait()
+    assert action.status == "completed"
+
+    log.info("waiting for csi to settle")
+    unit = model.applications["kubernetes-master"].units[0]
+    await retry_async_with_timeout(
+        verify_ready, (unit, "po", ["csi-rbdplugin"]), timeout_msg="CSI pods not ready!"
+    )
+    # create pod that writes to a pv from ceph
+    await validate_storage_class(model, "ceph-xfs", "Ceph")
+    await validate_storage_class(model, "ceph-ext4", "Ceph")
+    if check_cephfs:
+        await validate_storage_class(model, "cephfs", "Ceph")
+    # cleanup
+    tasks = {
+        model.applications["ceph-mon"].destroy(),
+        model.applications["ceph-osd"].destroy(),
+    }
+    if check_cephfs:
+        tasks.add(model.applications["ceph-fs"].destroy())
+    (done1, pending1) = await asyncio.wait(tasks)
+    for task in done1:
+        # read and ignore any exception so that it doesn't get raised
+        # when the task is GC'd
+        task.exception()
 
 
 # @pytest.mark.asyncio
