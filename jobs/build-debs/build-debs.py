dcf6fcbd85d2810fded371f2e7011236f88eb929
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2020-10-06 12:52:17 -0400

    cleanup build debs cloning
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

--- jobs/build-debs/build-debs.py
+++ jobs/build-debs/build-debs.py
@@ -20,29 +20,35 @@ VERSION_PPA = {
 
 
 class KubernetesRepo:
-    def __init__(self, k8s_version):
-        self.base_url = "git+ssh://cdkbot@git.launchpad.net"
+    def __init__(self, k8s_version, git_user):
+        self.git_user = git_user
+        self.base_url = f"git+ssh://{git_user}@git.launchpad.net"
         self.k8s_version = k8s_version
 
     def get_kubernetes_source(self):
         """Clones internal kubernetes source"""
         click.echo("Downloading internal kubernetes source repository")
-        cmd_ok(f"git clone {self.base_url}/k8s-internal-mirror")
-        cmd_ok(f"git checkout {self.k8s_version}", cwd="k8s-internal-mirror")
+        if not Path("k8s-internal-mirror").exists():
+            cmd_ok(f"git clone {self.base_url}/k8s-internal-mirror")
+            cmd_ok(f"git checkout {self.k8s_version}", cwd="k8s-internal-mirror")
+        else:
+            cmd_ok(f"git pull --ff-only", cwd="k8s-internal-mirror")
 
     def get_packaging_repos(self):
         """Downloads the required packaging repos"""
         click.echo("Downloading packaging repositories")
         for repo in DEB_REPOS:
-            click.echo(":: {self.base_url}/{repo}")
-            cmd_ok(f"git clone {self.base_url}/{repo}")
+            if not Path(repo).exists():
+                cmd = f"git clone {self.base_url}/{repo}"
+                click.echo(f":: {cmd}")
+                cmd_ok(cmd)
 
 
 class BuildRepo:
     def make_debs(self):
         """Builds the debian packaging for each component"""
         for repo in DEB_REPOS:
-            cmd_ok(f"cp -a {repo}/* k8s-internal-mirror/.")
+            cmd_ok(f"cp -a {repo}/* k8s-internal-mirror/.", shell=True)
             cmd_ok(f"debuild -us -uc", cwd="k8s-internal-mirror")
             cmd_ok(f"rm -rf debian")
 
@@ -94,7 +100,8 @@ def sync_tags():
 
 @cli.command()
 @click.option("--version", help="Kubernetes tag to build", required=True)
-def build_debs(version):
+@click.option("--git-user", help="Git repo user", required=True, default="cdkbot")
+def build_debs(version, git_user):
     _fmt_rel = version.lstrip("v")
     parsed_version = version
     try:
@@ -105,7 +112,7 @@ def build_debs(version):
 
     PPA = VERSION_PPA[parsed_version]
     click.echo(f"Selecting PPA: {PPA}")
-    repo = KubernetesRepo(version)
+    repo = KubernetesRepo(version, git_user)
     repo.get_kubernetes_source()
     repo.get_packaging_repos()
 
