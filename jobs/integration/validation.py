a858a4ae52b949365abbbbf70f087116aa1f4387
George Kraft <george.kraft@canonical.com>
2021-01-08 14:43:41 -0600

    Update validate-ck-calico and validate-ck-tigera-secure-ee to reuse VPCs (#679)
    * Update validate-ck-calico and validate-ck-tigera-secure-ee to reuse VPCs
    
    * Run inv format

--- jobs/integration/validation.py
+++ jobs/integration/validation.py
@@ -574,9 +574,14 @@ async def test_network_policies(model, tools):
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip_apps(["calico"])
 async def test_ipv6(model, tools):
-    master = model.applications["kubernetes-master"].units[0]
+    master_app = model.applications["kubernetes-master"]
+    master_config = await master_app.get_config()
+    service_cidr = master_config["service-cidr"]["value"]
+    if all(ipaddress.ip_network(cidr).version != 6 for cidr in service_cidr.split(",")):
+        pytest.skip("kubernetes-master not configured for IPv6")
+
+    master = master_app.units[0]
     kubectl(
         "create -f - << EOF{}EOF".format(
             """
