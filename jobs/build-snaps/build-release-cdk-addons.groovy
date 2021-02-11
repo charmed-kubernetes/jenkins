465eef2211d8ec46b9008bbb610a638ebf9729ca
Kevin W Monroe <kevin.monroe@canonical.com>
2021-02-11 11:55:21 -0600

    remove motd workaround (it now lives in the infra-maint playbook)

--- jobs/build-snaps/build-release-cdk-addons.groovy
+++ jobs/build-snaps/build-release-cdk-addons.groovy
@@ -91,10 +91,6 @@ pipeline {
             steps {
                 echo "Setting K8s version: ${kube_version} and K8s ersion: ${kube_ersion}"
                 sh """
-                    # motd-news-config is broken on some workers, but it is needed by ubuntu-server
-                    # for non-intel snap builds. force install it with default config.
-                    sudo apt-get -o DPkg::Options::=--force-confdef -y install motd-news-config || true
-
                     cd cdk-addons
                     make KUBE_VERSION=${kube_version} prep
                     ARCHES="amd64 arm64 ppc64le s390x"
