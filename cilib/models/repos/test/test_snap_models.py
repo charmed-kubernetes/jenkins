325bfb1e75eaf99008fb35a76feecb08718035d1
Kevin W Monroe <kevin.monroe@canonical.com>
2021-04-14 09:01:51 -0500

    update repo unit test for new release

--- cilib/models/repos/test/test_snap_models.py
+++ cilib/models/repos/test/test_snap_models.py
@@ -11,12 +11,12 @@ def test_repo_matches_model():
 def test_get_proper_tracks():
     """Test that proper tracks are associated with known versions"""
     repo_model = SnapKubeletRepoModel()
-    repo_model.version = "1.20"
-    assert repo_model.tracks == [
-        "1.20/stable",
-        "1.20/candidate",
-        "1.20/beta",
-        "1.20/edge",
-    ]
     repo_model.version = "1.21"
-    assert repo_model.tracks == ["1.21/candidate", "1.21/beta", "1.21/edge"]
+    assert repo_model.tracks == [
+        "1.21/stable",
+        "1.21/candidate",
+        "1.21/beta",
+        "1.21/edge",
+    ]
+    repo_model.version = "1.22"
+    assert repo_model.tracks == ["1.22/edge"]
