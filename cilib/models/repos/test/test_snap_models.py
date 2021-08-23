23ca03b4a218184f5b4f27789cac3f1f3fb5d17b
Kevin W Monroe <kevin.monroe@canonical.com>
2021-08-23 11:28:24 -0500

    adjust enums to push 1.22 snaps to 1.22/stable

--- cilib/models/repos/test/test_snap_models.py
+++ cilib/models/repos/test/test_snap_models.py
@@ -11,12 +11,12 @@ def test_repo_matches_model():
 def test_get_proper_tracks():
     """Test that proper tracks are associated with known versions"""
     repo_model = SnapKubeletRepoModel()
-    repo_model.version = "1.21"
-    assert repo_model.tracks == [
-        "1.21/stable",
-        "1.21/candidate",
-        "1.21/beta",
-        "1.21/edge",
-    ]
     repo_model.version = "1.22"
-    assert repo_model.tracks == ["1.22/candidate", "1.22/beta", "1.22/edge"]
+    assert repo_model.tracks == [
+        "1.22/stable",
+        "1.22/candidate",
+        "1.22/beta",
+        "1.22/edge",
+    ]
+    repo_model.version = "1.23"
+    assert repo_model.tracks == ["1.23/edge"]
