f5788d51af3aed36f1f697cd168a5d8bf40e5402
Adam Dyess <adam.dyess@canonical.com>
2022-01-21 10:46:47 -0600

    overcome github rate-limiting using basicauth (#790)
    * Don't assume that 'master' is the default branch
    
    * overcome github rate-limiting using basicauth

--- jobs/build-charms/charms.py
+++ jobs/build-charms/charms.py
@@ -239,7 +239,10 @@ class BuildEntity:
         else:
             src_path = self.checkout_path
 
-        default_branch = default_gh_branch(opts["downstream"], ignore_errors=True)
+        auth = (os.environ.get("CDKBOT_GH_USR"), os.environ.get("CDKBOT_GH_PSW"))
+        default_branch = default_gh_branch(
+            opts["downstream"], ignore_errors=True, auth=auth
+        )
 
         if "branch" in opts:
             self.charm_branch = opts["branch"]
