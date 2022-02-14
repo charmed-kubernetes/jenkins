8d429cabe1c0bafce25c0a7013d40c39676f1428
Kevin W Monroe <kevin.monroe@canonical.com>
2022-02-14 17:37:31 -0600

    new eks snap job (#812)
    * templatize eks snaps; build em like we do cdk-addons
    
    * make jenkins node configurable
    
    * drop unneeded env prefix; push snap source into container
    
    * build/fetch snaps from container; add upload step
    
    * deprecate old eks job

---
+++ jobs/build-snaps/build-release-eks-snaps.groovy
@@ -0,0 +1,154 @@
+@Library('juju-pipeline@master') _
+
+def kube_version = params.k8s_tag
+def kube_ersion = kube_version.substring(1)
+def lxc_name = env.JOB_NAME+"-"+env.BUILD_NUMBER
+
+pipeline {
+    agent {
+        label "${params.build_node}"
+    }
+    /* XXX: Global $PATH setting doesn't translate properly in pipelines
+     https://stackoverflow.com/questions/43987005/jenkins-does-not-recognize-command-sh
+     */
+    environment {
+        PATH = "${utils.cipaths}"
+        CK_SNAPS = "kubectl kubelet kubernetes-test kube-proxy"
+        EKS_BASE = "core20"
+        EKS_SUFFIX = "-eks"
+    }
+    options {
+        ansiColor('xterm')
+        timestamps()
+    }
+    stages {
+        stage('Setup User') {
+            /* Do this early; we want to fail fast if we don't have valid creds. */
+            steps {
+                sh """
+                    snapcraft logout
+                    snapcraft login --with /var/lib/jenkins/.config/snapcraft/snapcraft-cpc.cfg
+                    snapcraft whoami
+                """
+            }
+        }
+        stage('Setup Source') {
+            steps {
+                sh """
+                    CK_SNAP_BRANCH="${kube_version}"
+                    CK_SNAP_REPO_PREFIX="https://git.launchpad.net/snap-"
+
+                    for snap in ${CK_SNAPS}
+                    do
+                        CK_SNAP_REPO="\${CK_SNAP_REPO_PREFIX}\${snap}"
+                        EKS_SNAP="\${snap}${EKS_SUFFIX}"
+
+                        if git ls-remote --exit-code --heads \${CK_SNAP_REPO} \${CK_SNAP_BRANCH}
+                        then
+                            echo "Getting \${snap} from \${CK_SNAP_BRANCH} branch."
+                            git clone \${CK_SNAP_REPO} --branch \${CK_SNAP_BRANCH} --depth 1 \${EKS_SNAP}
+                        else
+                            echo "ERROR: \${CK_SNAP_BRANCH} branch not found in the \${CK_SNAP_REPO} repo."
+                            exit 1
+                        fi
+
+                        # eks snaps have a suffix and different base than ck; adjust snapcraft.yaml
+                        cd \${EKS_SNAP}
+                        sed -i -e "s/^name: \${snap}/name: \${EKS_SNAP}/" \
+                               -e "s/^base: .*/base: ${EKS_BASE}/" snapcraft.yaml
+                        cd -
+                    done
+                """
+            }
+        }
+        stage('Setup Build Container'){
+            steps {
+                /* override sh for this step:
+
+                 Needed because cilib.sh has some non POSIX bits.
+                 */
+                sh """#!/usr/bin/env bash
+                    . \${WORKSPACE}/cilib.sh
+
+                    ci_lxc_launch ubuntu:20.04 ${lxc_name}
+                    until sudo lxc shell ${lxc_name} -- bash -c "snap install snapcraft --classic"; do
+                        echo 'retrying snapcraft install in 3s...'
+                        sleep 3
+                    done
+
+                    for snap in ${CK_SNAPS}
+                    do
+                        EKS_SNAP="\${snap}${EKS_SUFFIX}"
+
+                        echo "Copying \${EKS_SNAP} into container."
+                        sudo lxc file push \${EKS_SNAP} ${lxc_name}/ -p -r
+                    done
+                """
+            }
+        }
+        stage('Build Snaps'){
+            steps {
+                sh """
+                    for snap in ${CK_SNAPS}
+                    do
+                        EKS_SNAP="\${snap}${EKS_SUFFIX}"
+
+                        echo "Building \${EKS_SNAP}."
+                        # NB: kubelet snapcraft.yaml defines an alias, but
+                        # aliases wont work in 'bash -c'. Define a func that
+                        # is functionally equivalent to the alias. Single quote
+                        # it because we need literal vars passed to 'bash -c'.
+                        sudo lxc shell ${lxc_name} -- bash -c \
+                            'set -a; add-arg() { \$SNAPCRAFT_PART_BUILD/shared/add-arg-to-configure-hook \$@; }; set +a; '"cd /\${EKS_SNAP};"' SNAPCRAFT_BUILD_ENVIRONMENT=host snapcraft'
+                    done
+                """
+            }
+        }
+        stage('Fetch Snaps'){
+            steps {
+                sh """
+                    BUILD_ARCH=\$(dpkg --print-architecture)
+                    for snap in ${CK_SNAPS}
+                    do
+                        EKS_SNAP="\${snap}${EKS_SUFFIX}"
+                        BUILT_SNAP="\${EKS_SNAP}_${kube_ersion}_\${BUILD_ARCH}.snap"
+
+                        echo "Fetching \${BUILT_SNAP}."
+                        sudo lxc file pull ${lxc_name}/\${EKS_SNAP}/\${BUILT_SNAP} .
+                    done
+                """
+            }
+        }
+        stage('Push Snaps'){
+            steps {
+                script {
+                    if(params.dry_run) {
+                        echo "Dry run; would have uploaded *.snap to ${params.channels}"
+                        sh "ls *eks*.snap"
+                    } else {
+                        sh """
+                            for snap in \$(ls *eks*.snap)
+                            do
+                                snapcraft -d upload \${snap} --release ${params.channels}
+                            done
+                        """
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            /* override sh for this step:
+
+             Needed because cilib.sh has some non POSIX bits.
+             */
+            sh """#!/usr/bin/env bash
+                . \${WORKSPACE}/cilib.sh
+
+                ci_lxc_delete ${lxc_name}
+                snapcraft logout
+            """
+        }
+    }
+}
