6a4e8531d2b0b3133182ce2bea1ec547896fb159
Adam Dyess <adam.dyess@canonical.com>
2022-08-15 11:45:35 -0500

    Prepare for 1.25 GA by publishing to the 1.25 tracks (#974)
    * Prepare for 1.25 GA by publishing to the 1.25 tracks
    
    * resolve linting issue
    
    * Update enums.py
    
    * create ordered track_list along with the track map
    
    * use go/1.19/candidate since the stable snap isn't available, open all but stable tracks for 1.25
    
    * Update enums.py
    
    `go/1.19/stable` is now available in the snap store as of 8/11/2022
    
    ```
    ❯ snap info go
    name:      go
    summary:   Go programming language compiler, linker, stdlib
    publisher: Michael Hudson-Doyle (mwhudson)
    store-url: https://snapcraft.io/go
    contact:   michael.hudson@ubuntu.com
    license:   BSD-3-Clause
    description: |
      This snap provides an assembler, compiler, linker, and compiled libraries
      for the Go programming language.
    snap-id: Md1HBASHzP4i0bniScAjXGnOII9cEK6e
    channels:
      latest/stable:    1.18.5           2022-08-11 (9952) 104MB classic
      latest/candidate: ↑                                        
      latest/beta:      ↑                                        
      latest/edge:      devel-c29be2d41c 2022-06-14 (9922) 109MB classic
      1.19/stable:      1.19             2022-08-11 (9951) 110MB classic
      1.19/candidate:   1.19             2022-08-10 (9951) 110MB classic
      1.19/beta:        ↑                                        
      1.19/edge:        ↑                                        
    ...
    ```

--- cilib/enums.py
+++ cilib/enums.py
@@ -44,6 +44,7 @@ K8S_CRI_TOOLS_SEMVER = "1.19"
 
 # Kubernetes build source to go version map
 K8S_GO_MAP = {
+    "1.25": "go/1.19/stable",
     "1.24": "go/1.18/stable",
     "1.23": "go/1.17/stable",
     "1.22": "go/1.16/stable",
@@ -58,17 +59,19 @@ K8S_GO_MAP = {
 # Snap k8s version <-> track mapping
 # Allows us to be specific in which tracks should get what major.minor and dictate when a release
 # should be put into the latest track.
-SNAP_K8S_TRACK_MAP = {
-    "1.16": ["1.16/stable", "1.16/candidate", "1.16/beta", "1.16/edge"],
-    "1.17": ["1.17/stable", "1.17/candidate", "1.17/beta", "1.17/edge"],
-    "1.18": ["1.18/stable", "1.18/candidate", "1.18/beta", "1.18/edge"],
-    "1.19": ["1.19/stable", "1.19/candidate", "1.19/beta", "1.19/edge"],
-    "1.20": ["1.20/stable", "1.20/candidate", "1.20/beta", "1.20/edge"],
-    "1.21": ["1.21/stable", "1.21/candidate", "1.21/beta", "1.21/edge"],
-    "1.22": ["1.22/stable", "1.22/candidate", "1.22/beta", "1.22/edge"],
-    "1.23": ["1.23/stable", "1.23/candidate", "1.23/beta", "1.23/edge"],
-    "1.24": ["1.24/stable", "1.24/candidate", "1.24/beta", "1.24/edge"],
-}
+SNAP_K8S_TRACK_LIST = [
+    ("1.16", ["1.16/stable", "1.16/candidate", "1.16/beta", "1.16/edge"]),
+    ("1.17", ["1.17/stable", "1.17/candidate", "1.17/beta", "1.17/edge"]),
+    ("1.18", ["1.18/stable", "1.18/candidate", "1.18/beta", "1.18/edge"]),
+    ("1.19", ["1.19/stable", "1.19/candidate", "1.19/beta", "1.19/edge"]),
+    ("1.20", ["1.20/stable", "1.20/candidate", "1.20/beta", "1.20/edge"]),
+    ("1.21", ["1.21/stable", "1.21/candidate", "1.21/beta", "1.21/edge"]),
+    ("1.22", ["1.22/stable", "1.22/candidate", "1.22/beta", "1.22/edge"]),
+    ("1.23", ["1.23/stable", "1.23/candidate", "1.23/beta", "1.23/edge"]),
+    ("1.24", ["1.24/stable", "1.24/candidate", "1.24/beta", "1.24/edge"]),
+    ("1.25", ["1.25/candidate", "1.25/beta", "1.25/edge"]),
+]
+SNAP_K8S_TRACK_MAP = dict(SNAP_K8S_TRACK_LIST)
 
 # Deb k8s version <-> ppa mapping
 DEB_K8S_TRACK_MAP = {
@@ -81,6 +84,7 @@ DEB_K8S_TRACK_MAP = {
     "1.22": "ppa:k8s-maintainers/1.22",
     "1.23": "ppa:k8s-maintainers/1.23",
     "1.24": "ppa:k8s-maintainers/1.24",
+    "1.25": "ppa:k8s-maintainers/1.25",
 }
 
 
