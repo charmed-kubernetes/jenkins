947966a3f95648cbee45c87c8286ca6e446a2cbc
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-11-10 09:49:15 +0200

    Put the CAPI cloud specific parts in the test setup (#1099)

--- jobs/microk8s/clusterapi/release.sh
+++ jobs/microk8s/clusterapi/release.sh
@@ -130,6 +130,17 @@ then
     kubectl apply -f bootstrap/bootstrap-components.yaml -f control-plane/control-plane-components.yaml
 
     # run integration tests
+    export AWS_REGION=us-east-1
+    export AWS_SSH_KEY_NAME=capi
+    export CONTROL_PLANE_MACHINE_COUNT=3
+    export WORKER_MACHINE_COUNT=3
+    export AWS_CREATE_BASTION=false
+    export AWS_PUBLIC_IP=false
+    export AWS_CONTROL_PLANE_MACHINE_FLAVOR=t3.large
+    export AWS_NODE_MACHINE_FLAVOR=t3.large
+    clusterctl generate cluster test-ci-cluster --from "bootstrap/templates/cluster-template-aws.yaml" --kubernetes-version 1.25.0 > cluster.yaml
+    export CLUSTER_MANIFEST_FILE=$PWD/cluster.yaml
+
     bash ./bootstrap/integration/test.sh
 
     # cleanup machine
