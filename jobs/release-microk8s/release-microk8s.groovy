7d1f99bf79a9cefee2388aa2fc14285c31142c20
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2022-12-08 08:24:03 +0200

    Fix flakiness in microk8s tests setup

--- jobs/release-microk8s/release-microk8s.groovy
+++ jobs/release-microk8s/release-microk8s.groovy
@@ -191,7 +191,9 @@ pipeline {
                                 juju run --unit ubuntu/0 "open-port 2049"
                                 juju expose ubuntu
 
-                                juju ssh -m "${juju_full_model}" --pty=true ubuntu/0 -- "sudo echo EFS_ID=\$EFS_ID | sudo tee -a /etc/environment"
+                                # Do some wating here to make sure the VM is ready.
+                                while ! juju show-status -m ${juju_full_model} --format short ubuntu/0 | grep active ; do sleep 10 ; echo waiting... ; done
+                                while ! juju ssh -m "${juju_full_model}" --pty=true ubuntu/0 -- "sudo echo EFS_ID=\$EFS_ID | sudo tee -a /etc/environment" ; do sleep 10 ; echo waiting...; done
                                 juju ssh -m "${juju_full_model}" --pty=true ubuntu/0 -- "sudo echo KUBERNETES_ADMIN_ARN=\$KUBERNETES_ADMIN_ARN | sudo tee -a /etc/environment"
                                 juju ssh -m "${juju_full_model}" --pty=true ubuntu/0 -- "sudo echo AWS_ACCESS_KEY_ID=\$AWS_ACCESS_KEY_ID | sudo tee -a /etc/environment"
                                 juju ssh -m "${juju_full_model}" --pty=true ubuntu/0 -- "sudo echo AWS_SECRET_ACCESS_KEY=\$AWS_SECRET_ACCESS_KEY | sudo tee -a /etc/environment"
