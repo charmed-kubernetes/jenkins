48ed1e7f06d41062485d8af6168969beef42d4c0
Adam Dyess <adam.dyess@canonical.com>
2023-04-21 14:31:36 -0500

    Adjustments to enhance debug logs when running validation tests

--- jobs/integration/conftest.py
+++ jobs/integration/conftest.py
@@ -207,11 +207,11 @@ class Tools:
                 await proc._feed_stdin(input)
 
         await asyncio.wait(
-            [
+            map(asyncio.create_task, [
                 _read_stream(proc.stdout, lambda _l: tee(_l, stdout, 1)),
                 _read_stream(proc.stderr, lambda _l: tee(_l, stderr, 2)),
                 _feed_stream(input=stdin),
-            ]
+            ])
         )
 
         return_code = await proc.wait()
