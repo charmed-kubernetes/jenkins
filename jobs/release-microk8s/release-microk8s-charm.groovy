3839ca49470b7928948bbf4fa7671ca77232768b
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-09-11 22:48:18 +0300

    Maybe now?

--- jobs/release-microk8s/release-microk8s-charm.groovy
+++ jobs/release-microk8s/release-microk8s-charm.groovy
@@ -17,6 +17,7 @@ pipeline {
         PATH                 = "/var/lib/jenkins/venvs/ci/bin:/snap/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin"
         JUJU_CLOUD           = "aws/us-east-1"
         CONTROLLER           = "release-microk8s-charm"
+        CHARMCRAFT_AUTH      = credentials('charmcraft_creds')
         NOTIFY_EMAIL         = credentials('microk8s_notify_email')
     }
     options {
