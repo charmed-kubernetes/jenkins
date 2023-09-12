26b84cf9353de2ca68c553fdba052a2044617935
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2023-09-12 13:43:45 +0300

    What is this tox_root

--- jobs/release-microk8s/release-microk8s-charm.groovy
+++ jobs/release-microk8s/release-microk8s-charm.groovy
@@ -44,7 +44,7 @@ pipeline {
                     """
                     try {
                         sh """
-                        . jobs/microk8s/.tox/py38/bin/activate
+                        # . jobs/microk8s/.tox/py38/bin/activate
                         cd jobs/microk8s/charms
                         DRY_RUN=${params.DRY_RUN} SKIP_TESTS=${params.SKIP_TESTS}\
                             BRANCH=${params.TESTS_BRANCH} REPOSITORY=${params.TESTS_REPOSITORY}\
