3b6ae34f986e7256f1d9954016afca8b852cc878
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2024-02-28 09:20:19 +0200

    Series

--- jobs/release-microk8s/release-microk8s.groovy
+++ jobs/release-microk8s/release-microk8s.groovy
@@ -122,7 +122,10 @@ pipeline {
 
                                 # We deploy 20.04 because the upgrade-path test deploys K8s 1.19 onwards
                                 # that requires old cgroups.
-                                juju deploy -m "${juju_full_model}" --constraints "${constraints}" ubuntu --base ubuntu@20.04
+                                # For juju 3.1:
+                                #juju deploy -m "${juju_full_model}" --constraints "${constraints}" ubuntu --base ubuntu@20.04
+                                For juju 2.9:
+                                juju deploy -m "${juju_full_model}" --constraints "${constraints}" ubuntu --series focal
 
                                 juju-wait -e "${juju_full_model}" -w
 
