6edfb437804b57cd24424a13f726d1f6ada2d661
Adam Dyess <adam.dyess@canonical.com>
2025-04-10 11:18:15 -0500

    Handle snap upgrade requirements across multiple ck releases (#1624)
    * Handle snap upgrade requirements across multiple ck releases
    
    * Expland bugfix-upgrade to upgrade from N-2 stable snaps

--- jobs/integration/conftest.py
+++ jobs/integration/conftest.py
@@ -333,7 +333,7 @@ async def model(request, tools):
         await tools.juju_wait()
         upgrade_snap_channel = request.config.getoption("--upgrade-snap-channel")
         upgrade_charm_channel = request.config.getoption("--upgrade-charm-channel")
-        if not upgrade_snap_channel and upgrade_charm_channel:
+        if not (upgrade_snap_channel and upgrade_charm_channel):
             raise Exception(
                 "Must have both snap and charm upgrade "
                 "channels set to perform upgrade prior to validation test."
